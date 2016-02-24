d88087332ace9f2f3f9102b0e52a618365a9397c
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2016-02-24 04:15:46 -0500

    Data type conversion!
    It *really* needs parentheses…

--- hs-to-coq/src/lib/HsToCoq/CLI.hs
+++ hs-to-coq/src/lib/HsToCoq/CLI.hs
@@ -5,7 +5,8 @@
 module HsToCoq.CLI (
   processFilesMain,
   dumpDataDecls, convertDataDecls,
-  processArgs
+  processArgs,
+  prettyPrint
   ) where
 
 import Data.Foldable
@@ -15,6 +16,7 @@ import Control.Monad.IO.Class
 
 import Data.Data
 
+import System.IO
 import System.Environment
 
 import GHC
@@ -22,9 +24,17 @@ import DynFlags
 
 import HsToCoq.Util.GHC
 import HsToCoq.Util.Messages
+import HsToCoq.PrettyPrint
+import HsToCoq.Coq.Gallina
 import HsToCoq.DataDecl
 import HsToCoq.ProcessFiles
 import HsToCoq.ConvertData
+
+prettyPrint :: MonadIO m => Doc -> m ()
+prettyPrint = liftIO . displayIO stdout . renderPretty 0.67 120
+
+prettyPrintLn :: MonadIO m => Doc -> m ()
+prettyPrintLn doc = prettyPrint doc *> liftIO (putStrLn "")
 
 processArgs :: GhcMonad m => m (DynFlags, [FilePath])
 processArgs = do
@@ -44,9 +54,9 @@ dumpDataDecls lmod = case getDataDecls lmod :: [DataDecl' RdrName] of
 
 convertDataDecls :: (Data a, GhcMonad m) => a -> m ()
 convertDataDecls lmod = case getDataDecls lmod :: [DataDecl' RdrName] of
-  [] -> liftIO $ putStrLn "No data type declarations to convert."
-  ds -> do liftIO $ putStrLn "Converted data type declarations:"
-           mapM_ (liftIO . putStr <=< convert) ds
+  [] -> liftIO $ putStrLn "-- No data type declarations to convert."
+  ds -> do liftIO $ putStrLn "-- Converted data type declarations:"
+           mapM_ (prettyPrintLn . (<> line) . renderGallina <=< convertDataDecl') ds
 
 processFilesMain :: GhcMonad m => (Located (HsModule RdrName) -> m a) -> m ()
 processFilesMain f = uncurry (traverse_ . processFile f) =<< processArgs
--- hs-to-coq/src/lib/HsToCoq/ConvertData.hs
+++ hs-to-coq/src/lib/HsToCoq/ConvertData.hs
@@ -1,24 +1,42 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections, LambdaCase, PatternSynonyms, OverloadedLists #-}
 
-module HsToCoq.ConvertData (showName, readDataDecls, convert) where
+module HsToCoq.ConvertData (
+  -- * Conversion
+  convertDataDecl', convertType, convertLType,
+  -- ** Names
+  showRdrName, showName,
+  -- ** Internal
+  convertDataDefn, convertConDecl, convertLHsTyVarBndrs,
+  -- * Input
+  readDataDecls,
+  -- * Coq construction
+  pattern App1, pattern Var, appList
+  ) where
 
 import Data.Foldable
-import Data.List
+import Data.Traversable
+import Data.List.NonEmpty (NonEmpty(..), nonEmpty)
 
+import Control.Monad
 import Control.Monad.IO.Class
-import Control.Monad.Writer
 
-import GHC
+import GHC hiding (Name)
+import qualified GHC
 import RdrName
 import OccName
 import Panic
 
+import HsToCoq.Util.Patterns
 import HsToCoq.Util.GHC
+import HsToCoq.Coq.Gallina
 import HsToCoq.DataDecl
 import HsToCoq.ProcessFiles
 
-showName :: GhcMonad m => RdrName -> m String
-showName = ghcSDoc . pprOccName . rdrNameOcc
+showRdrName :: GhcMonad m => RdrName -> m String
+showRdrName = ghcSDoc . pprOccName . rdrNameOcc
+
+showName :: GhcMonad m => GHC.Name -> m String
+showName = showRdrName . nameRdrName
 
 readDataDecls :: GhcMonad m => DynFlags -> FilePath -> m [DataDecl' RdrName]
 readDataDecls dflags file =   processFile (pure . getDataDecls) dflags file
@@ -26,28 +44,146 @@ readDataDecls dflags file =   processFile (pure . getDataDecls) dflags file
                                     . ProgramError $ file ++ ": no parse" )
                                     pure
 
-data Type' = Type
-          deriving (Eq, Ord, Show)
+-- Module-local
+conv_unsupported :: MonadIO m => String -> m a
+conv_unsupported what = liftIO . throwGhcExceptionIO . ProgramError $ what ++ " unsupported"
 
-data Binder = Plain { bName :: String }
-            | Typed { bName :: String, bType :: Type' }
-            deriving (Eq, Ord, Show)
+pattern App1 f x = App f (PosArg x :| Nil)
+pattern Var v    = Qualid (Bare v)
 
-data Inductive = Inductive { iName   :: String
-                           , iParams :: [Binder]
-                           , iType   :: Type'
-                           , iCons   :: [Constructor] }
+appList :: Term -> [Arg] -> Term
+appList f xs = case nonEmpty xs of
+                Nothing  -> f
+                Just xs' -> App f xs'
 
-data Constructor = Constructor { cName :: String
-                               , cType :: Type' }
+convertType :: GhcMonad m => HsType RdrName -> m Term
+convertType (HsForAllTy _explicit _ _tvs _ctx _ty) =
+  conv_unsupported "forall" -- FIXME
 
-convert :: GhcMonad m => DataDecl' RdrName -> m String
-convert DataDecl'{tcdDataDefn' = HsDataDefn{..}, ..} = execWriterT $ do
-  let lname = lift . showName . unLoc
-  dname <- lname tcdLName'
-  tell $ "Inductive " ++ dname ++ " :=\n"
-  for_ dd_cons $ \(L _ ConDecl{..}) -> do
-    tell "  | "
-    tell . intercalate ", " =<< traverse lname con_names
-    tell " : \n"
-  tell ".\n"
+convertType (HsTyVar tv) =
+  Var <$> ghcPpr tv -- TODO Check module part?
+
+convertType (HsAppTy ty1 ty2) =
+  App1 <$> convertLType ty1 <*> convertLType ty2
+
+convertType (HsFunTy ty1 ty2) =
+  Arrow <$> convertLType ty1 <*> convertLType ty2
+
+convertType (HsListTy ty) =
+  App1 (Var "list") <$> convertLType ty
+
+convertType (HsPArrTy _ty) =
+  conv_unsupported "parallel arrays (`[:a:]')"
+
+convertType (HsTupleTy tupTy tys) = do
+  case tupTy of
+    HsUnboxedTuple           -> conv_unsupported "unboxed tuples"
+    HsBoxedTuple             -> pure ()
+    HsConstraintTuple        -> conv_unsupported "constraint tuples"
+    HsBoxedOrConstraintTuple -> pure () -- Sure, it's boxed, why not
+  case tys of
+    []   -> pure $ Var "unit"
+    [ty] -> convertLType ty
+    _    -> foldl1 (\x t -> App (Var "prod") [PosArg x, PosArg t]) <$> traverse convertLType tys
+
+convertType (HsOpTy _ty1 _op _ty2) =
+  conv_unsupported "binary operators" -- FIXME
+
+convertType (HsParTy ty) =
+  Parens <$> convertLType ty
+
+convertType (HsIParamTy _ _) =
+  conv_unsupported "implicit parameters"
+
+convertType (HsEqTy _ty1 _ty2) =
+  conv_unsupported "type equality" -- FIXME
+
+convertType (HsKindSig ty k) =
+  HasType <$> convertLType ty <*> convertLType k
+
+convertType (HsQuasiQuoteTy _) =
+  conv_unsupported "quasiquoters"
+
+convertType (HsSpliceTy _ _) =
+  conv_unsupported "Template Haskell"
+
+convertType (HsDocTy ty _doc) =
+  convertLType ty
+
+convertType (HsBangTy _bang ty) =
+  convertLType ty -- Strictness annotations are ignored
+
+convertType (HsRecTy _fields) =
+  conv_unsupported "record types" -- FIXME
+
+convertType (HsCoreTy _) =
+  conv_unsupported "[internal] embedded core types"
+
+convertType (HsExplicitListTy PlaceHolder tys) =
+  foldr (\ty l -> App (Var "cons") [PosArg ty, PosArg l]) (Var "nil") <$> traverse convertLType tys
+
+convertType (HsExplicitTupleTy _PlaceHolders tys) =
+  case tys of
+    []   -> pure $ Var "tt"
+    [ty] -> convertLType ty
+    _    -> foldl1 (\x t -> App (Var "pair") [PosArg x, PosArg t]) <$> traverse convertLType tys
+
+convertType (HsTyLit lit) =
+  case lit of
+    HsNumTy _ int | int >= 0  -> pure . Num $ fromInteger int
+                  | otherwise -> conv_unsupported "negative type-level integers"
+    HsStrTy _ _               -> conv_unsupported "type-level strings"
+
+convertType (HsWrapTy _ _) =
+  conv_unsupported "[internal] wrapped types" 
+
+convertType HsWildcardTy =
+  pure Underscore
+
+convertType (HsNamedWildcardTy _) =
+  conv_unsupported "named wildcards"
+
+convertLType :: GhcMonad m => LHsType RdrName -> m Term
+convertLType = convertType . unLoc
+
+type Constructor = (Ident, [Binder], Maybe Term)
+
+convertLHsTyVarBndrs :: GhcMonad m => LHsTyVarBndrs RdrName -> m [Binder]
+convertLHsTyVarBndrs (HsQTvs kvs tvs) = do
+  kinds <- traverse (fmap (Inferred . Ident) . ghcPpr) kvs
+  types <- for (map unLoc tvs) $ \case
+             UserTyVar   tv   -> Inferred . Ident <$> ghcPpr tv
+             KindedTyVar tv k -> Typed <$> (pure . Ident <$> ghcPpr tv) <*> convertLType k
+  pure $ kinds ++ types
+
+convertConDecl :: GhcMonad m => Term -> ConDecl RdrName -> m [Constructor]
+convertConDecl curType (ConDecl lnames _explicit lqvs lcxt ldetails lres _doc _old) = do
+  unless (null $ unLoc lcxt) $ conv_unsupported "constructor contexts"
+  names  <- traverse (ghcPpr . unLoc) lnames
+  params <- convertLHsTyVarBndrs lqvs
+  resTy  <- case lres of
+              ResTyH98       -> pure curType
+              ResTyGADT _ ty -> convertLType ty
+  args   <- traverse convertLType $ hsConDeclArgTys ldetails
+  pure $ map (, params, Just $ foldr Arrow resTy args) names
+  
+convertDataDefn :: GhcMonad m => Term -> HsDataDefn RdrName -> m (Term, [Constructor])
+convertDataDefn curType (HsDataDefn _nd lcxt _ctype ksig cons _derivs) = do
+  unless (null $ unLoc lcxt) $ conv_unsupported "data type contexts"
+  (,) <$> maybe (pure $ Sort Type) convertLType ksig
+      <*> (concat <$> traverse (convertConDecl curType . unLoc) cons)
+
+convertDataDecl' :: GhcMonad m => DataDecl' RdrName -> m Inductive
+convertDataDecl' (DataDecl' name tvs defn _fvs) = do
+  coqName       <- ghcPpr $ unLoc name
+  params        <- convertLHsTyVarBndrs tvs
+  let binderNames = foldMap $ \case
+                      Inferred x    -> [x]
+                      Typed xs _    -> toList xs
+                      BindLet _ _ _ -> []
+      nameArgs    = map $ PosArg . \case
+                      Ident x        -> Var x
+                      UnderscoreName -> Underscore
+      curType     = appList (Var coqName) . nameArgs $ binderNames params
+  (resTy, cons) <- convertDataDefn curType defn
+  pure $ Inductive [IndBody coqName params resTy cons]

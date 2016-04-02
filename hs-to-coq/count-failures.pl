ed8a91fa55c94c53a0f2e4d37a6c73a3e9b84545
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2016-04-02 13:14:48 -0400

    [core-coq] Script for counting conversion failures
    A Perl script.  I know, I know.

---
+++ hs-to-coq/count-failures.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl -w
+use strict;
+use v5.10;
+
+use List::Util qw(sum0 max);
+
+my %failures = ();
+
+{
+  my $cur_comment = undef;
+  while (<>) {
+    $_ = $cur_comment.$_ if $cur_comment;
+    $cur_comment = s/^\(\*(?!.+\*\)\n)(.*)\n/(*$1/ ? $_ : undef;
+    tr/ / /s;
+    ++$failures{$1} if /^\(\*.+failed: +(.+?) +unsupported.*\*\)$/;
+  }
+}
+
+my @failures = ();
+while (my ($reason, $count) = each %failures) {
+  push @failures, [$count,$reason];
+}
+@failures = sort { $b->[0] <=> $a->[0] || $a->[1] cmp $b->[1] } @failures;
+
+sub format_result(_;$) {
+  my ($count, $reason) = ref($_[0]) eq 'ARRAY' ? @{$_[0]} : @_;
+  return sprintf '%4d %s', $count, $reason;
+}
+
+my $total = sum0(map { $_->[0] } @failures);
+my @lines = map format_result, @failures;
+my $rule  = '-' x max(map length, @lines);
+
+say foreach @lines;
+say $rule;
+say (format_result $total, 'TOTAL');

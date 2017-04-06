980acc51ea61216019828aa2a9f6c96becc1a42a
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2017-04-06 12:33:20 -0400

    Add `isNoPostTcExpr`

--- hs-to-coq/src/lib/HsToCoq/Util/GHC/HsExpr.hs
+++ hs-to-coq/src/lib/HsToCoq/Util/GHC/HsExpr.hs
@@ -1,14 +1,26 @@
-module HsToCoq.Util.GHC.HsExpr (module HsExpr, isNoSyntaxExpr) where
+module HsToCoq.Util.GHC.HsExpr (
+  module HsExpr,
+  isNoSyntaxExpr,
+  isNoPostTcExpr,
+  isGenLitString
+) where
 
 import FastString
 import HsLit
 import HsExpr
 import TcEvidence (HsWrapper(..))
 
+isGenLitString :: String -> HsExpr id -> Bool
+isGenLitString str (HsLit (HsString "" fstr)) = fsLit str == fstr
+isGenLitString _   _                          = False
+
 isNoSyntaxExpr :: SyntaxExpr id -> Bool
-isNoSyntaxExpr SyntaxExpr{ syn_expr = HsLit (HsString "" str)
+isNoSyntaxExpr SyntaxExpr{ syn_expr      = expr
                          , syn_arg_wraps = []
                          , syn_res_wrap  = WpHole } =
-  str == fsLit "noSyntaxExpr"
+  isGenLitString "noSyntaxExpr" expr
 isNoSyntaxExpr _ =
   False
+
+isNoPostTcExpr :: PostTcExpr -> Bool
+isNoPostTcExpr = isGenLitString "noPostTcExpr"

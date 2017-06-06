3e1274ce2b048b14acb9d06808692a4e0300f869
Stephanie Weirich <sweirich@cis.upenn.edu>
2017-06-06 10:45:48 -0400

    fixed Expr test

--- hs-to-coq/examples/tests/Expr.hs
+++ hs-to-coq/examples/tests/Expr.hs
@@ -10,11 +10,8 @@ data Expr b
   = Var   Id
   | App   (Expr b) (Arg b)
   | Lam   b (Expr b)
-  | Case  (Expr b) b Type [Alt b]       -- See #case_invariants#
   deriving Data
 
 -- | Type synonym for expressions that occur in function argument positions.
 -- Only 'Arg' should contain a 'Type' at top level, general 'Expr' should not
 type Arg b = Expr b
-
-type Alt b = (AltCon, [b], Expr b)

efb6959f065fbc89f3cb7ae30a80ee548efea989
Joachim Breitner <mail@joachim-breitner.de>
2017-08-04 13:29:42 -0400

    Make PatternGuard test case more recursion checker friendly

--- hs-to-coq/examples/tests/PatternGuard.hs
+++ hs-to-coq/examples/tests/PatternGuard.hs
@@ -12,14 +12,15 @@ data Pair a b = Pair a b
 data List a = Nil | Cons a (List a)
 
 
-take :: Int -> List a -> List a
-take n _      | n <= 0 =  Nil
-take _ Nil             =  Nil
-take n (Cons x xs)     =  Cons x (take (n-1) xs)
+-- Reverse order of arguments to make the termination checker happy
+take :: List a -> Int -> List a
+take _ n      | n <= 0 =  Nil
+take Nil _             =  Nil
+take (Cons x xs) n     =  Cons x (take xs (n-1))
 
 
-take2 :: Int -> List a -> List a
-take2 n x = case Pair n x of
+take2 :: List a -> Int -> List a
+take2 x n = case Pair n x of
         Pair n _      | n <= 0 -> Nil
         Pair _ Nil             -> Nil
-        Pair n (Cons x xs)     -> Cons x (take2 (n-1) xs)
+        Pair n (Cons x xs)     -> Cons x (take2 xs (n-1))

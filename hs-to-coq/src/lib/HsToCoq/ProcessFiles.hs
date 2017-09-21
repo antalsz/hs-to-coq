04851f349e0a39ed30a8520bc9be33dcdda4fceb
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2017-09-21 19:35:28 -0400

    Deleted some unused functions

--- hs-to-coq/src/lib/HsToCoq/ProcessFiles.hs
+++ hs-to-coq/src/lib/HsToCoq/ProcessFiles.hs
@@ -1,42 +1,12 @@
 {-# LANGUAGE LambdaCase #-}
 
-module HsToCoq.ProcessFiles (
-  processFiles,
-  processFileFlags, parseFileFlags
-) where
+module HsToCoq.ProcessFiles (processFiles) where
 
 import Control.Lens
-
 import Data.Foldable
-
 import Control.Monad
-import Control.Monad.IO.Class
-
 import System.FilePath
-
 import GHC
-import HeaderInfo
-import DynFlags
-
-import HsToCoq.Util.Messages
-
-import HsToCoq.Util.GHC
-
-parseFileFlags :: GhcMonad m
-               => ([Located String] -> [Located String] -> m ())
-               -> DynFlags -> FilePath
-               -> m DynFlags
-parseFileFlags handleExtra dflags file = do
-  (fileDflags, restOpts, optWarns) <-
-    parseDynamicFilePragma dflags =<< liftIO (getOptionsFromFile dflags file)
-  handleExtra restOpts optWarns
-  void $ setSessionDynFlags fileDflags
-  getSessionDynFlags
-
-processFileFlags :: (GhcMonad m, MonadIO m) => DynFlags -> FilePath -> m DynFlags
-processFileFlags = parseFileFlags $ \restOpts optWarns -> do
-  printAllIfPresent unLoc "Leftover option" restOpts
-  printAllIfPresent unLoc "Option warning"  optWarns
 
 processFiles :: GhcMonad m => [FilePath] -> m (Maybe [TypecheckedModule])
 processFiles files = do

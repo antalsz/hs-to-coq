9ecebc62d36dd6863d2c0e5f17088767cc538f30
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2017-09-22 13:59:56 -0400

    `MissingValue` means `missingValue`

--- hs-to-coq/src/lib/HsToCoq/Coq/Preamble.hs
+++ hs-to-coq/src/lib/HsToCoq/Coq/Preamble.hs
@@ -26,9 +26,9 @@ staticPreamble = T.unlines
  , "Unset Printing Implicit Defensive."
  , ""
  -- TODO: Shouldn't be duplicated in every file
- , "(* Let us be a bit explicit by having multiple axoims around *)"
- , "(* This one is for untranslatable expressions: *)"
+ -- Let us be a bit explicit by having multiple axioms around.
+ -- This one is for untranslatable expressions:
  , "Local Axiom missingValue : forall {a}, a."
- , "(* This one is for pattern match failures: *)"
+ -- This one is for pattern match failures:
  , "Local Axiom patternFailure : forall {a}, a."
  ]

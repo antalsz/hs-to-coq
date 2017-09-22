b57340bf2a64e2e0c6f2f876845773dd3f644efc
Christine Rizkallah <criz@seas.upenn.edu>
2017-09-22 17:38:08 -0400

    original haskell compiler is not correct. Corrected compiler and correctness proof

--- hs-to-coq/examples/compiler/Compiler.hs
+++ hs-to-coq/examples/compiler/Compiler.hs
@@ -19,10 +19,11 @@ type Code = [Op]
 data Op = PUSH Int | ADD
           deriving Show
 
-exec :: Code -> Stack -> Stack
-exec [] s = s
+exec :: Code -> Stack -> Maybe Stack
+exec [] s = Just s
 exec (PUSH n : c) s = exec c (n : s)
 exec (ADD : c) (m : n : s) = exec c (n+m : s)
+exec (ADD : c) _ = Nothing
 
 comp :: Expr -> Code
 comp (Val n) = [PUSH n]
--- hs-to-coq/examples/compiler/Proofs.v
+++ hs-to-coq/examples/compiler/Proofs.v
@@ -1,2 +1,39 @@
 Require Import Prelude.
 Require Import Compiler.
+
+
+Import ListNotations.
+
+Definition apply {aT} {rT} (f : aT -> rT) x u :=
+  if u is Some y then f y else x.
+
+Definition bind {aT} {rT} (f : aT -> option rT) :=
+  apply f None.
+
+
+Lemma exec_app_distributivity: forall c d s,
+    exec (c ++ d) s = bind (exec d) (exec c s).
+Proof.
+  induction c; intros; [auto|].
+  destruct a; [simpl; auto|].
+  destruct s; [auto|].
+  destruct s; [auto|].
+  simpl. auto.
+Qed.
+
+Lemma comp_correct_helper: forall e s,
+    exec (comp e) s =  Some (eval e :: s).
+Proof.
+  induction e; intros; auto.
+  simpl.
+  rewrite exec_app_distributivity.
+  rewrite IHe1.
+  simpl.
+  rewrite exec_app_distributivity.
+  rewrite IHe2.
+  auto.
+Qed.
+
+Theorem comp_correct: forall e,
+    exec (comp e) [] = Some [eval e].
+  by (intros; apply comp_correct_helper; auto).

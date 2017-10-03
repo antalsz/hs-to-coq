818ef5a025f8df9cd28ce71d2fe0263a36a9a14b
Joachim Breitner <mail@joachim-breitner.de>
2017-10-03 15:31:45 -0400

    Remove duplicate proofs of `distributivity`

--- hs-to-coq/examples/compiler/ProofsOrig.v
+++ hs-to-coq/examples/compiler/ProofsOrig.v
@@ -3,38 +3,7 @@ Require Import CompilerOrig.
 
 Import ListNotations.
 
-(* AKA comp_correct_book *)
-Lemma distributivity : forall c s d,
-    exec c s <> patternFailure ->
-    exec (app c d) s =  exec d (exec c s).
-Proof.
-  induction c; intros; auto.
-  destruct a.
-  - simpl. rewrite IHc. auto.
-    simpl in H. auto.
-  - simpl. destruct s. simpl in H. contradiction.
-    destruct s. simpl in H. contradiction.
-    simpl in H.
-    rewrite IHc.
-    auto. auto.
-Qed.
-
-Lemma comp_correct_book : forall e s,
-    exec (comp e) s = eval e :: s.
-Proof.
-  induction e; intros s.
-  auto.
-  simpl.
-  rewrite distributivity.
-  rewrite IHe1.
-  rewrite distributivity.
-  rewrite IHe2.
-  simpl.
-  auto.
-  admit.
-  admit.
-Admitted.
-
+(* Alternative proof that goes through with patternFailures *)
 
 Lemma comp_correct_helper: forall e s d,
     exec (comp e ++ d) s =  exec d (eval e :: s).
@@ -56,24 +25,41 @@ Proof.
   apply comp_correct_helper.
 Qed.
 
+
+
+
 (* Nice, but actually useless: The precondition cannot be
    shown for concrete [c] and [s]. *)
-Lemma comp_correct_book: forall c s d,
+(* AKA comp_correct_book *)
+Lemma distributivity: forall c s d,
     exec c s <> patternFailure ->
     exec (c ++ d) s =  exec d (exec c s).
 Proof.
-  induction c; intros.
-  * reflexivity.
-  * destruct a.
-    - simpl.
-      rewrite IHc.
-      reflexivity.
-      simpl in H. assumption.
-    - simpl.
-      destruct s.
-      + simpl in H. contradict H. reflexivity.
-      + destruct s.
-        ** simpl in H. contradict H. reflexivity.
-        ** rewrite IHc. reflexivity.
-           simpl in H. assumption.
+  induction c; intros; auto.
+  destruct a.
+  - simpl. rewrite IHc. auto.
+    simpl in H. auto.
+  - simpl. destruct s. simpl in H. contradiction.
+    destruct s. simpl in H. contradiction.
+    simpl in H.
+    rewrite IHc.
+    auto. auto.
 Qed.
+
+
+Lemma comp_correct_book : forall e s,
+    exec (comp e) s = eval e :: s.
+Proof.
+  induction e; intros s.
+  auto.
+  simpl.
+  rewrite distributivity.
+  rewrite IHe1.
+  rewrite distributivity.
+  rewrite IHe2.
+  simpl.
+  auto.
+  admit. (* exec (comp e2) (eval e1 :: s) <> patternFailure *)
+  admit. (* Cannot prove exec (comp e1) s <> patternFailure *)
+Abort.
+

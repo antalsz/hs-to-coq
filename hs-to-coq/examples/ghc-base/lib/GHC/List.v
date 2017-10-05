674fbc6dce0bd7d5a1ddb8c65e074eafbe88162a
Joachim Breitner <mail@joachim-breitner.de>
2017-10-05 12:27:32 -0400

    Update ghc-base/lib in the repository

--- hs-to-coq/examples/ghc-base/lib/GHC/List.v
+++ hs-to-coq/examples/ghc-base/lib/GHC/List.v
@@ -130,11 +130,14 @@ Definition lenAcc {a} : list a -> GHC.Num.Int -> GHC.Num.Int :=
   fix lenAcc arg_236__ arg_237__
         := match arg_236__ , arg_237__ with
              | nil , n => n
-             | (cons _ ys) , n => lenAcc ys (GHC.Num.op_zp__ n #1)
+             | (cons _ ys) , n => lenAcc ys (GHC.Num.op_zp__ n (GHC.Num.fromInteger 1))
            end.
 
 Definition length {a} : list a -> GHC.Num.Int :=
-  fun arg_240__ => match arg_240__ with | xs => lenAcc xs #0 end.
+  fun arg_240__ =>
+    match arg_240__ with
+      | xs => lenAcc xs (GHC.Num.fromInteger 0)
+    end.
 
 Definition lengthFB {x}
     : x -> (GHC.Num.Int -> GHC.Num.Int) -> GHC.Num.Int -> GHC.Num.Int :=
@@ -142,7 +145,7 @@ Definition lengthFB {x}
     match arg_229__ , arg_230__ with
       | _ , r => fun arg_231__ =>
                    match arg_231__ with
-                     | a => r (GHC.Num.op_zp__ a #1)
+                     | a => r (GHC.Num.op_zp__ a (GHC.Num.fromInteger 1))
                    end
     end.
 
@@ -174,32 +177,34 @@ Definition or : list bool -> bool :=
            end.
 
 Definition prel_list_str : GHC.Base.String :=
-  &"Prelude.".
+  GHC.Base.hs_string__ "Prelude.".
 
 Definition tooLarge {a} : GHC.Num.Int -> a :=
   fun arg_65__ =>
     GHC.Base.errorWithoutStackTrace (Coq.Init.Datatypes.app prel_list_str
-                                                            &"!!: index too large").
+                                                            (GHC.Base.hs_string__ "!!: index too large")).
 
 Definition errorEmptyList {a} : GHC.Base.String -> a :=
   fun arg_1__ =>
     match arg_1__ with
       | fun_ => GHC.Base.errorWithoutStackTrace (Coq.Init.Datatypes.app prel_list_str
-                                                                        (Coq.Init.Datatypes.app fun_ &": empty list"))
+                                                                        (Coq.Init.Datatypes.app fun_
+                                                                                                (GHC.Base.hs_string__
+                                                                                                ": empty list")))
     end.
 
 Definition foldl1 {a} : (a -> a -> a) -> list a -> a :=
   fun arg_203__ arg_204__ =>
     match arg_203__ , arg_204__ with
       | f , (cons x xs) => GHC.Base.foldl f x xs
-      | _ , nil => errorEmptyList &"foldl1"
+      | _ , nil => errorEmptyList (GHC.Base.hs_string__ "foldl1")
     end.
 
 Definition foldl1' {a} : (a -> a -> a) -> list a -> a :=
   fun arg_198__ arg_199__ =>
     match arg_198__ , arg_199__ with
       | f , (cons x xs) => GHC.Base.foldl' f x xs
-      | _ , nil => errorEmptyList &"foldl1'"
+      | _ , nil => errorEmptyList (GHC.Base.hs_string__ "foldl1'")
     end.
 
 Definition foldr1 {a} : (a -> a -> a) -> list a -> a :=
@@ -210,7 +215,7 @@ Definition foldr1 {a} : (a -> a -> a) -> list a -> a :=
                      := match arg_148__ with
                           | (cons x nil) => x
                           | (cons x xs) => f x (go xs)
-                          | nil => errorEmptyList &"foldr1"
+                          | nil => errorEmptyList (GHC.Base.hs_string__ "foldr1")
                         end in
              go
     end.
@@ -218,7 +223,7 @@ Definition foldr1 {a} : (a -> a -> a) -> list a -> a :=
 Definition init {a} : list a -> list a :=
   fun arg_245__ =>
     match arg_245__ with
-      | nil => errorEmptyList &"init"
+      | nil => errorEmptyList (GHC.Base.hs_string__ "init")
       | (cons x xs) => let init' :=
                          fix init' arg_247__ arg_248__
                                := match arg_247__ , arg_248__ with
@@ -229,7 +234,7 @@ Definition init {a} : list a -> list a :=
     end.
 
 Definition lastError {a} : a :=
-  errorEmptyList &"last".
+  errorEmptyList (GHC.Base.hs_string__ "last").
 
 Definition last {a} : list a -> a :=
   fun arg_254__ =>
@@ -243,14 +248,14 @@ Definition last {a} : list a -> a :=
 Definition maximum {a} `{(GHC.Base.Ord a)} : list a -> a :=
   fun arg_208__ =>
     match arg_208__ with
-      | nil => errorEmptyList &"maximum"
+      | nil => errorEmptyList (GHC.Base.hs_string__ "maximum")
       | xs => foldl1 GHC.Base.max xs
     end.
 
 Definition minimum {a} `{(GHC.Base.Ord a)} : list a -> a :=
   fun arg_212__ =>
     match arg_212__ with
-      | nil => errorEmptyList &"minimum"
+      | nil => errorEmptyList (GHC.Base.hs_string__ "minimum")
       | xs => foldl1 GHC.Base.min xs
     end.
 
@@ -258,11 +263,11 @@ Definition tail {a} : list a -> list a :=
   fun arg_260__ =>
     match arg_260__ with
       | (cons _ xs) => xs
-      | nil => errorEmptyList &"tail"
+      | nil => errorEmptyList (GHC.Base.hs_string__ "tail")
     end.
 
 Definition product {a} `{(GHC.Num.Num a)} : list a -> a :=
-  GHC.Base.foldl GHC.Num.op_zt__ #1.
+  GHC.Base.foldl GHC.Num.op_zt__ (GHC.Num.fromInteger 1).
 
 Definition reverse {a} : list a -> list a :=
   fun arg_96__ =>
@@ -364,7 +369,7 @@ Definition strictUncurryScanr {a} {b} {c} : (a -> b -> c) -> a * b -> c :=
     end.
 
 Definition sum {a} `{(GHC.Num.Num a)} : list a -> a :=
-  GHC.Base.foldl GHC.Num.op_zp__ #0.
+  GHC.Base.foldl GHC.Num.op_zp__ (GHC.Num.fromInteger 0).
 
 Definition takeWhileFB {a} {b}
     : (a -> bool) -> (a -> b -> b) -> b -> a -> b -> b :=
---
+++ hs-to-coq/examples/ghc-base/lib/Prelude.v
@@ -0,0 +1,17 @@
+Require Export GHC.Base.  (* exports Prim, Num, Char *)
+Require Export GHC.List.
+Require Export GHC.Enum.
+Require Export GHC.Real.
+Require Export GHC.Unicode.
+Require Export GHC.Word.
+Require Export GHC.Int.
+
+Require Export Data.Char.
+Require Export Data.Bits.
+Require Export Data.Tuple.
+Require Export Data.Maybe.
+Require Export Data.Bool.
+Require Export Data.Function.
+Require Export Data.Ord.
+(* Require Export Data.OldList. *)
+Require Export Data.Monoid.

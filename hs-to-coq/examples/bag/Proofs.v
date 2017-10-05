b57ccff592fc759b1dbe084b52a0a9d5abf32286
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2017-10-05 17:42:57 -0400

    Second bag proof :-)

--- hs-to-coq/examples/bag/Proofs.v
+++ hs-to-coq/examples/bag/Proofs.v
@@ -1,8 +1,13 @@
 Require Import Bag.
+Require Import Coq.Lists.List.
+Import ListNotations.
+
 From mathcomp Require Import ssreflect.
 
 Theorem bag_cons_ok {A} (x : A) (b : Bag A) :
-  bagToList (consBag x b) = cons x (bagToList b).
-Proof.
-  elim: b => //=.
-Qed.
+  bagToList (consBag x b) = x :: bagToList b.
+Proof. elim: b => //. Qed.
+
+Theorem unit_bag_ok {A} (x : A) :
+  bagToList (unitBag x) = [ x ].
+Proof. reflexivity. Qed.

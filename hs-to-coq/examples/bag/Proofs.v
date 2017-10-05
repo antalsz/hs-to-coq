4aa4845aea9629be2e1f5fc88a7ac7789fc60aa7
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2017-10-05 17:41:12 -0400

    First bag proof!!!!!

--- hs-to-coq/examples/bag/Proofs.v
+++ hs-to-coq/examples/bag/Proofs.v
@@ -1,1 +1,8 @@
 Require Import Bag.
+From mathcomp Require Import ssreflect.
+
+Theorem bag_cons_ok {A} (x : A) (b : Bag A) :
+  bagToList (consBag x b) = cons x (bagToList b).
+Proof.
+  elim: b => //=.
+Qed.

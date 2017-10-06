f56944f0f902dc116fbffba0e154a313bce44901
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2017-10-06 13:39:09 -0400

    Add "for" as a reserved word

--- hs-to-coq/src/lib/HsToCoq/ConvertHaskell/Variables.hs
+++ hs-to-coq/src/lib/HsToCoq/ConvertHaskell/Variables.hs
@@ -45,7 +45,7 @@ escapeReservedNames :: Ident -> Ident
 escapeReservedNames x =
   fromMaybe x . getFirst $
     foldMap (First . flip tryEscapeReservedWord x)
-            (T.words "Set Type Prop fun fix forall return mod as cons pair nil")
+            (T.words "Set Type Prop fun fix forall return mod as cons pair nil for")
     <> if | T.all (== '.') x  -> pure $ T.map (const '∘') x
           | T.all (== '∘') x  -> pure $ "⟨" <> x <> "⟩"
 -- these type operators aren't parsed by the renaming file

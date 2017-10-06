632554fbc3630fb3c5a5354321c7796eab1a77cc
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2017-10-05 23:02:42 -0400

    [bag] Factor out list proofs

--- hs-to-coq/examples/bag/Proofs.v
+++ hs-to-coq/examples/bag/Proofs.v
@@ -1,50 +1,11 @@
 Require Import Bag.
-Require GHC.List.
 
-Require Import Coq.Program.Basics.
 Require Import Coq.Lists.List.
 Import ListNotations.
-Open Scope program_scope.
+Require Import ListUtils.
 
 From mathcomp Require Import ssreflect ssrfun ssrbool.
 Set Bullet Behavior "Strict Subproofs".
-
-(**** Lists *****)
-
-Theorem fold_right_cons {A} (tail l : list A) :
-  fold_right cons tail l = l ++ tail.
-Proof.
-  elim: l => [| x l IH] //=.
-  by rewrite IH.
-Qed.
-
-Corollary fold_right_cons_nil {A} (l : list A) :
-  fold_right cons [] l = l.
-Proof. by rewrite fold_right_cons app_nil_r. Qed.
-
-Theorem fold_right_map {A B C} (f : B -> C -> C) (z : C) (g : A -> B) (l : list A) :
-  fold_right f z (map g l) = fold_right (f ∘ g) z l.
-Proof.
-  elim: l => [|x l IH] //=.
-  by rewrite IH.
-Qed.
-
-Theorem fold_right_fold_right {A} (f : A -> A -> A) (z : A) (l1 l2 : list A) :
-  associative f -> left_id z f ->
-  fold_right f (fold_right f z l2) l1 = f (fold_right f z l1) (fold_right f z l2).
-Proof.
-  move=> f_assoc z_left_id.
-  elim: l1 => [|x1 l1 IH] //=.
-  by rewrite IH f_assoc.
-Qed.
-
-Theorem filter_app {A} (p : A -> bool) (l1 l2 : list A) :
-  filter p (l1 ++ l2) = filter p l1 ++ filter p l2.
-Proof.
-  elim: l1 => [|x1 l1 IH] //=.
-  case: (p x1) => //=.
-  by rewrite IH.
-Qed.
 
 (**** Bag invariant *****)
 
@@ -140,13 +101,9 @@ Proof. by elim: l => [| x l IH] //=; rewrite /bagToList /= fold_right_cons_nil. 
    there are multiple equivalent bags -- for example, `ListBag [x]` and
    `UnitBag x`. *)
 
-Lemma app_null {A} (xs ys : list A) :
-  GHC.List.null (xs ++ ys) = GHC.List.null xs && GHC.List.null ys.
-Proof. case: xs => //=. Qed.
-
 Theorem isEmptyBag_ok {A} (b : Bag A) :
   well_formed_bag b ->
-  isEmptyBag b = GHC.List.null (bagToList b).
+  isEmptyBag b = null (bagToList b).
 Proof.
   elim: b => [| x | l IHl r IHr | xs] //=.
   - rewrite eval_wf_TwoBags => /and4P [nel ner wfl wfr] *.

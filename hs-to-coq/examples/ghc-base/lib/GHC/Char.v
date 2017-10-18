2dcba84273b418196dde46f7d2686de5896b949b
Joachim Breitner <mail@joachim-breitner.de>
2017-10-18 13:46:57 -0400

    Use symbolic links to get `manual/` into `lib/`
    so that there is no doubt what file to edit, one can rummage in `lib/`
    without losing changes, and the `git diff`’s are simpler to read.

--- hs-to-coq/examples/ghc-base/lib/GHC/Char.v
+++ hs-to-coq/examples/ghc-base/lib/GHC/Char.v
@@ -1,15 +1,1 @@
-Require Import GHC.Num.
-
-(* Characters *)
-
-Require Import NArith.
-Definition Char := N.
-Bind Scope char_scope   with N.
-
-(* Notation for literal characters in Coq source. *)
-Require Import Coq.Strings.Ascii.
-Definition hs_char__ : Ascii.ascii -> Char := N_of_ascii.
-Notation "'&#' c" := (hs_char__ c) (at level 1, format "'&#' c").
-
-Definition chr : Int -> Char := Z.to_N.
-Definition ord : Char -> Int := Z.of_N.
+../../manual/GHC/Char.v
--- hs-to-coq/examples/ghc-base/lib/GHC/Tuple.v
+++ hs-to-coq/examples/ghc-base/lib/GHC/Tuple.v
@@ -1,14 +1,1 @@
-Open Scope type_scope.
-
-Definition pair_type   := (fun (x y :Type) => (x * y)).
-
-Definition op_Z2T__ := (fun {a} {b} (x:a) (y:b) => (x, y)).
-Definition op_Z3T__ := (fun {a} {b} {c} (x:a) (y:b) (z:c) => (x, y, z)).
-Definition op_Z4T__ := (fun {a} {b} {c} {d} (x:a) (y:b) (z:c) (w:d) => (x, y, z, w)).
-Definition op_Z5T__ := (fun {a} {b} {c} {d} {e}
-                          (x:a) (y:b) (z:c) (w:d) (v:e) => (x, y, z, w,v)).
-Definition op_Z6T__ := (fun {a} {b} {c} {d} {e} {f}
-                          (x:a) (y:b) (z:c) (w:d) (v:e) (u:f) => (x, y, z, w, v,u)).
-Definition op_Z7T__ := (fun {a} {b} {c} {d} {e} {f} {g}
-                          (x:a) (y:b) (z:c) (w:d) (v:e) (u:f) (t:g) =>
-                          (x, y, z, w, v, u, t)).
+../../manual/GHC/Tuple.v

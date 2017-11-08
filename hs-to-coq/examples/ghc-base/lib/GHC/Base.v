ebfb66c7bc2dda645714d7e67931f44b7d699999
Joachim Breitner <mail@joachim-breitner.de>
2017-11-08 17:12:33 -0500

    Define eqlist with a local loop
    to make the termination checker happy

--- hs-to-coq/examples/ghc-base/lib/GHC/Base.v
+++ hs-to-coq/examples/ghc-base/lib/GHC/Base.v
@@ -485,12 +485,13 @@ Module comparison_OrderedType___ <: OrderedType.OrderedType.
 End comparison_OrderedType___.
 
 (* TODO: are these available in a library somewhere? *)
-Fixpoint eqlist {a} `{Eq_ a} (xs :  list a) (ys : list a) : bool :=
-    match xs , ys with
-    | nil , nil => true
-    | x :: xs' , y :: ys' => andb (x == y) (eqlist xs' ys')
-    | _ ,  _ => false
-    end.
+Fixpoint eqlist {a} `{Eq_ a} : list a -> list a -> bool :=
+	fix eqlist xs ys :=
+	    match xs , ys with
+	    | nil , nil => true
+	    | x :: xs' , y :: ys' => andb (x == y) (eqlist xs' ys')
+	    | _ ,  _ => false
+	    end.
 
 Fixpoint compare_list {a} `{Ord a} (xs :  list a) (ys : list a) : comparison :=
     match xs , ys with

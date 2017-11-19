e0e344f8e7285d85b15b53e64dfdf993a40ae32e
Stephanie Weirich <sweirich@cis.upenn.edu>
2017-11-19 13:11:33 -0500

    polymorphism should be in Type, not Set

--- hs-to-coq/examples/ghc-base/module-edits/GHC/List/preamble.v
+++ hs-to-coq/examples/ghc-base/module-edits/GHC/List/preamble.v
@@ -7,14 +7,14 @@ Require Import ZArith.BinInt.
 (* Hand-translated version of the prelude definitions
    of these functions. *)
 
-Fixpoint take {a:Set} (n:Z) (xs:list a) : list a :=
+Fixpoint take {a:Type} (n:Z) (xs:list a) : list a :=
   if (n <=? 0)%Z then nil
   else match xs with
        | nil => nil
        | cons y ys => cons y (take (n - 1) ys)
        end.
 
-Fixpoint drop {a:Set} (n:Z) (xs:list a) : list a :=
+Fixpoint drop {a:Type} (n:Z) (xs:list a) : list a :=
   if (n <=? 0)%Z then xs
   else match xs with
        | nil => nil
@@ -23,7 +23,7 @@ Fixpoint drop {a:Set} (n:Z) (xs:list a) : list a :=
 
 (* TODO: mark impossible case with default. *)
 
-Fixpoint scanr {a b:Set} (f : a -> b -> b) (q0 : b)
+Fixpoint scanr {a b:Type} (f : a -> b -> b) (q0 : b)
         (xs: list a) :=
  match xs with
  | nil => (cons q0 nil)
@@ -34,13 +34,13 @@ Fixpoint scanr {a b:Set} (f : a -> b -> b) (q0 : b)
 end.
 
 
-Definition splitAt {a : Set}(n:Z)(xs:list a) :=
+Definition splitAt {a : Type}(n:Z)(xs:list a) :=
   (take n xs, drop n xs).
 
-Definition replicate {a: Set}(n:Z): a -> list a.
+Definition replicate {a:Type}(n:Z): a -> list a.
 destruct (0 <=? n)%Z eqn:E; [|exact (fun _ => nil)].
 apply Zle_bool_imp_le in E.
-apply natlike_rec with (P := fun _ => a -> list a)(x := n).
+apply natlike_rec2 with (P := fun _ => a -> list a)(z := n).
 - exact (fun _ => nil).
 - intros n1 Pf rec x. exact (cons x (rec x)).
 - exact E.

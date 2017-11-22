b403c66153aa6ada820a2485d36bb8d850c3f752
Joachim Breitner <mail@joachim-breitner.de>
2017-11-22 15:19:49 -0500

    Revert "Refactor Variables.var (to be able to make sense of it)"
    This reverts commit 420b6ec53c903c55cf8ddbf0fa612c3ef385d8ad.

--- hs-to-coq/src/lib/HsToCoq/ConvertHaskell/Variables.hs
+++ hs-to-coq/src/lib/HsToCoq/ConvertHaskell/Variables.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiWayIf, OverloadedStrings, FlexibleContexts, LambdaCase #-}
+{-# LANGUAGE MultiWayIf, OverloadedStrings, FlexibleContexts #-}
 
 module HsToCoq.ConvertHaskell.Variables (
   -- * Generate variable names
@@ -14,6 +14,7 @@ import Data.Monoid hiding ((<>))
 import Data.Maybe
 import qualified Data.Text as T
 
+import Control.Applicative
 import Control.Monad
 
 import GHC hiding (Name)
@@ -50,7 +51,7 @@ escapeReservedNames x =
 -- these type operators aren't parsed by the renaming file
           | x == "(->)"       -> pure $ ("arrow")
           | x == "(,)"        -> pure $ ("pair_type")
-          | x == "#."         -> pure $ ("hash_compose")  -- Data.Foldable
+          | x == "#."       -> pure $ ("hash_compose")  -- Data.Foldable
 -- Maybe add this as part of an Int# solution? But don't want to
 -- always replace these, if we make "Int#" a notation for "Int_h"
 --          | T.isInfixOf "#" x -> pure $ T.replace "#" "_h" x
@@ -67,33 +68,31 @@ freeVar = fmap freeVar' . ghcPpr
 var' :: ConversionMonad m => HsNamespace -> Ident -> m Ident
 var' ns x = use $ renamed ns x . non (escapeReservedNames x)
 
-
--- Removes the module prefix if this is in the current module
-localize :: ConversionMonad m => Ident -> m Ident
-localize q = do
-  thisModM <- fmap moduleNameText <$> use currentModule
-  pure $ case (identToQualid q, thisModM) of
-    (Just (Qualified m b), Just thisMod) | qualidToIdent m == thisMod -> b
-    _                                                                 -> q
-
 -- This is dishonest: it should return a Qualid for qualified names
 var :: ConversionMonad m => HsNamespace -> GHC.Name -> m Ident
 var ns name = do
+  thisModM <- fmap moduleNameText <$> use currentModule
   let nameModM = moduleNameText . moduleName <$> nameModule_maybe name
-  let base = T.pack . occNameString $ nameOccName name
-  let escaped_base = escapeReservedNames base
 
-  case nameModM of
-    Just mod -> do
-        -- The name has a module, so it (or at least may be) a global name
-        let escapedQualName = mod <> "." <> escaped_base
-            qualName        = mod <> "." <> base
-        -- So look the _unescaped_ qualified name up in the renaming
-        use (renamed ns qualName) >>= \case
-            Just r ->  localize r
-            Nothing -> localize escapedQualName
-        -- A local name, no module. Not much to do here.
-    Nothing -> pure escaped_base
+  let mod = nameModM
+
+      qual | thisModM == nameModM = ""
+           | otherwise            = fromMaybe "" mod
+
+      base = T.pack . occNameString $ nameOccName name
+
+  let localize q = case (identToQualid q, thisModM) of
+        (Just (Qualified m b), Just thisMod) | qualidToIdent m == thisMod -> b
+        _                                                                 -> q
+
+      "" <.> b = b
+      q  <.> b = q <> "." <> b
+      tryRenamed = fmap (fmap localize) . use . renamed ns
+      (<<|>>) = liftA2 (<|>)
+
+  fmap (fromMaybe $ qual <.> escapeReservedNames base)
+    $     maybe (pure Nothing) (tryRenamed . (<.> base)) mod
+    <<|>> tryRenamed (qual <.> base)
 
 recordField :: (ConversionMonad m, HasOccName name, OutputableBndr name) => name -> m Ident
 recordField = var' ExprNS <=< ghcPpr -- TODO Check module part?

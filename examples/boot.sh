6116d2ebf99ec204e1f8eb05102cb324492db224
Joachim Breitner <mail@joachim-breitner.de>
2017-11-30 17:28:08 -0500

    Make boot skript rebuild everything

--- examples/boot.sh
+++ examples/boot.sh
@@ -12,17 +12,18 @@ cd $(dirname $0)
 
 echo "Cleaning everything"
 make -C tests clean
+make -C base-tests clean
 make -C successors clean
 make -C compiler clean
 make -C rle clean
 make -C bag clean
-make -C ghc clean
+make -C ghc/lib clean
 make -C ../base-thy clean
 
 if [[ -e base-src/base ]]
 then
 	echo "Regenerating ../base"
-	rm -rf ../base
+	make -C base-src clean
 	make -C base-src
 else
 	echo "Rebuiding ../base"
@@ -32,3 +33,33 @@ fi
 
 echo "Building ../base-thy"
 make -C ../base-thy
+
+echo "Building examples"
+make -C base-tests
+make -C successors
+make -C compiler
+make -C rle
+make -C bag
+make -C quicksort
+
+if [[ -e ghc/ghc ]]
+then
+	echo "Regenerating ghc"
+	make -C ghc clean
+	make -C ghc
+else
+	echo "Rebuiding ghc/lib"
+	make -C ghc/lib clean
+	make -C ghc/lib
+fi
+
+if [[ -e containers/containers ]]
+then
+	echo "Regenerating containers"
+	make -C containers clean
+	make -C containers
+else
+	echo "Rebuiding containers/lib"
+	make -C containers/lib clean
+	make -C containers/lib
+fi

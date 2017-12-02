69fcd0f87e2d3749c6873e1e557f904c530e40a4
Stephanie Weirich <sweirich@cis.upenn.edu>
2017-12-02 12:54:10 -0500

    fix up proof

--- examples/lambda/Lambda.hs
+++ examples/lambda/Lambda.hs
@@ -8,23 +8,24 @@ data Expr
   | Lam Name Expr
   | App Expr Expr
 
-
+-- Data.Foldable.maximum is a partial function (fails when the list is empty)
+-- So we use a total version that requires a nonempty list.
 maxName :: Name -> [Name] -> Name
 maxName n ns = foldr max n ns
 
 maxVar :: Expr -> Int
-maxVar (Var n) = n
+maxVar (Var n)       = n
 maxVar (Let n e1 e2) = maxName n [maxVar e1, maxVar e2]
-maxVar (Lam n e) = maxName  n [maxVar e]
-maxVar (App e1 e2) = maxName (maxVar e1) [maxVar e2]
+maxVar (Lam n e)     = maxName  n [maxVar e]
+maxVar (App e1 e2)   = maxName (maxVar e1) [maxVar e2]
 
 fresh :: Expr -> Int
 fresh e = 1 + maxVar e
 
 anf :: Expr -> Expr
-anf (Var n) = Var n
+anf (Var n)          = Var n
 anf (Let n rhs body) = Let n (anf rhs) (anf body)
-anf (Lam n body) = Lam n (anf body)
-anf (App f (Var n)) = App (anf f) (Var n)
-anf (App f e) = Let v (anf e) (App (anf f) (Var v))
+anf (Lam n body)     = Lam n (anf body)
+anf (App f (Var n))  = App (anf f) (Var n)
+anf (App f e)        = Let v (anf e) (App (anf f) (Var v))
   where v = fresh f
--- examples/lambda/Proofs.v
+++ examples/lambda/Proofs.v
@@ -1,6 +1,10 @@
 Require Import Prelude.
 Require Import Lambda.
 
+Require Import Recdef.
+
+Set Bullet Behavior "Strict Subproofs".
+
 Definition isVal (e : Expr) : Prop :=
   match e with
     | Var _ => True
@@ -21,13 +25,13 @@ Definition rename : Name -> Name -> Expr -> Expr :=
 
 
 
-Fixpoint depth e := match e with
+Fixpoint depth e : nat := match e with
   | Var v' => 0
   | Lam v' e => S (depth e)
   | App e1 e2 =>
-    PeanoNat.Nat.max (S (depth e1)) (S (depth e2))
+    Nat.max (S (depth e1)) (S (depth e2))
   | Let v' rhs body =>
-    PeanoNat.Nat.max (S (depth rhs)) (S (depth body))
+    Nat.max (S (depth rhs)) (S (depth body))
 end.
 
 
@@ -50,7 +54,7 @@ Proof.
     auto.
 Qed.
 
-Require Import Recdef.
+
 
 
 Function subst v arg body {measure depth body} :=
@@ -98,6 +102,7 @@ Fixpoint small_step_fun (t : Expr) : option Expr :=
                   | Some t2' => Some (App t1 t2')
                   | _ => match t2 with
                         | App _ _ => None
+                        | Let _ _ _ => None
                         | _ => Some (subst v t2 t3)
                         end
                   end
@@ -130,78 +135,71 @@ Proof.
   intros H; inversion H.
 Qed.
 
+Lemma fun_step : forall  k m m', (k = depth m)%nat ->
+  small_step_fun m = Some m' -> small_step m m'.
+Proof.
+  intro k.
+  induction (lt_wf k) as [n _ IH].
+  intros m m' Heq SS.
+  destruct m; simpl in *; subst; try solve [inversion SS].
+  - (* let *)
+    destruct m1.
+    + inversion SS.
+      eapply ss_let; simpl; auto.
+    + destruct (small_step_fun (Let n m1_1 m1_2)) eqn:J; inversion SS.
+      eapply ss_let_search.
+      eapply (IH (depth (Let n m1_1 m1_2))); auto.
+      zify; omega.
+    + inversion SS.
+      eapply ss_let; simpl; auto.
+    + destruct (small_step_fun (App m1_1 m1_2)) eqn:J; inversion SS.
+      eapply ss_let_search.
+      eapply (IH (depth (App m1_1 m1_2))); auto.
+      zify; omega.
+  -  (* app *)
+    destruct m1.
+    + destruct (small_step_fun m2) eqn:J; inversion SS.
+      eapply ss_app_val.
+      simpl; auto.
+      eapply (IH (depth m2)); simpl; auto.
+    + destruct (small_step_fun (Let n m1_1 m1_2)) eqn:J; inversion SS.
+      eapply ss_app_search.
+      eapply (IH (depth (Let n m1_1 m1_2))); auto.
+      zify; omega.
+    + destruct (small_step_fun m2) eqn:J; inversion SS.
+      eapply ss_app_val.
+      simpl; auto.
+      eapply (IH (depth m2)); auto.
+      zify; omega.
+      destruct m2;
+        try solve [inversion SS; eapply ss_beta; simpl; auto].
+    + destruct (small_step_fun (App m1_1 m1_2)) eqn:J; inversion SS.
+      eapply ss_app_search.
+      eapply (IH (depth (App m1_1 m1_2))); auto.
+      zify; omega.
+Qed.
 
 Lemma small_step_small_step_fun : forall m m',
   small_step m m' <-> small_step_fun m = Some m'.
 Proof.
   split.
-  - induction 1.
-    + simpl. rewrite isVal_not_step; auto.
-      destruct arg; auto. inversion H.
-    + simpl.
-      destruct rhs; auto;
+  - induction 1; simpl.
+    + (* beta *) rewrite isVal_not_step; auto.
+      destruct arg; auto; inversion H.
+    + (* let *) destruct rhs; auto;
         inversion H; subst.
-    + inversion H; subst.
-      * simpl in IHsmall_step.
-        simpl.
-        destruct (small_step_fun arg).
-        -- inversion IHsmall_step; auto.
-        -- destruct arg; auto. inversion IHsmall_step.
-      * simpl in IHsmall_step.
-        simpl.
-        destruct rhs0; auto.
-        -- destruct (small_step_fun (Let n rhs0_1 rhs0_2)).
-           inversion IHsmall_step. auto.
-        -- inversion IHsmall_step.
-      * destruct (small_step_fun (App rhs0_1 rhs0_2));
-          inversion IHsmall_step; auto.
-      * simpl in IHsmall_step.
-        simpl.
-        destruct rhs0.
-        -- inversion IHsmall_step. auto.
-        -- destruct (small_step_fun (Let n rhs0_1 rhs0_2)).
-           inversion IHsmall_step. auto.
-        -- inversion IHsmall_step.
-        -- inversion IHsmall_step. auto.
-        -- destruct (small_step_fun (App rhs0_1 rhs0_2));
-             inversion IHsmall_step; auto.
-        - simpl in IHsmall_step.
-           simpl.
-           destruct m; try solve [inversion H0].
-           ++ destruct (small_step_fun (Let n0 m1 m2));
-                inversion IHsmall_step; subst; auto.
-           ++ destruct (small_step_fun (App m1 m2));
-                inversion IHsmall_step; auto.
-        -- simpl in IHsmall_step.
-           simpl.
-           destruct m; try solve [inversion H0].
-           ++ destruct (small_step_fun n);
-                inversion IHsmall_step; subst; auto.
-           ++ destruct (small_step_fun n);
-                inversion IHsmall_step; subst; auto.
-              destruct n;
-                try solve [inversion H1| inversion H3].
-              inversion H3. auto.
-        -- destruct m;
-             try solve [inversion IHsmall_step];
-                 simpl in IHsmall_step;
-                 simpl.
-           ++ destruct m1; inversion IHsmall_step; auto.
-              ** destruct (small_step_fun
-                             (Let n1 m1_1 m1_2));
-                   inversion IHsmall_step; auto.
-              ** destruct (small_step_fun
-                             (App m1_1 m1_2));
-                   inversion IHsmall_step; auto.
-        + simpl. simpl in IHsmall_step.
-              intuition.
-              ** inversion IHsmall_step. subst. auto.
-              **  admit.
-                  admit.
-                  admit.
-Admitted.
+    + (* let search *) destruct rhs; auto; try solve [inversion H];
+        rewrite IHsmall_step; auto.
+    + (* app search *)
+      destruct m; try solve [inversion IHsmall_step];
+      rewrite IHsmall_step; auto.
+    + (* app val *)
+      destruct m; try solve [inversion H];
+      rewrite IHsmall_step; auto.
+  - eapply fun_step. eauto.
+Qed.
 
-
+(*
 Inductive rtc {A} (R:A -> A -> Prop) : A -> A -> Prop :=
 | rtc_refl : forall a, rtc R a a
 | rtc_step : forall a b c, R a b -> rtc R b c -> rtc R a c.
@@ -209,6 +207,7 @@ Inductive rtc {A} (R:A -> A -> Prop) : A -> A -> Prop :=
 Notation "R ^*" := (rtc R) (at level 1, format "R ^*").
 
 Definition nf (x: Expr): Prop := ~ exists y, small_step x y.
+*)
 
 (*
 Inductive beta_equiv : Expr -> Expr -> Prop :=

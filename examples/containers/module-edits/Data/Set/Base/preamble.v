e66d11648b86a19b1dfd035e7710836d977d300f
Yao Li <hnkfliyao@gmail.com>
2017-12-05 18:09:04 -0500

    Much more methods are translated in Data.Set.
    Thanks Joachim for showing me how to work with `termination` and `nonterminating` in edits.

--- examples/containers/module-edits/Data/Set/Base/preamble.v
+++ examples/containers/module-edits/Data/Set/Base/preamble.v
@@ -1,8 +1,8 @@
-Require Import GHC.Base.
+Require GHC.Base.
 
 Module GHC.
   Module Err.
     Axiom undefined : forall {a}, a.
-    Axiom error : forall {a}, String -> a.
+    Axiom error : forall {a}, GHC.Base.String -> a.
   End Err.
 End GHC.

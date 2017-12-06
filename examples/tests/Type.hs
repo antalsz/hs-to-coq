13abe5ec621f4ff0af6c6c65130be19e7a306855
Stephanie Weirich <sweirich@cis.upenn.edu>
2017-12-06 07:05:55 -0800

    Don't use Coq keywords as module names

---
+++ examples/tests/Type.hs
@@ -0,0 +1,7 @@
+module Type where
+
+-- Shouldn't name a module with a Coq keyword
+-- The error shows up in TypeClient, though
+
+f :: Int -> Int
+f = id
---
+++ examples/tests/TypeClient.hs
@@ -0,0 +1,8 @@
+module TypeClient where
+
+-- Can't refer to modules named Type
+-- Should rename the module
+import Type
+
+g :: Int
+g = f 3

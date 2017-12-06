b2aef809587c2c4864626be21988b2d2964fa340
Joachim Breitner <mail@joachim-breitner.de>
2017-12-06 16:30:45 -0500

    List all the proof obligations for the SetProofs and MapProofs

--- examples/containers/theories/SetProofs.v
+++ examples/containers/theories/SetProofs.v
@@ -1,5 +1,5 @@
+Require Import GHC.Base.
 Require Import Data.Set.Base.
-
 Require Import Coq.FSets.FSetInterface.
 
 Module Foo (E : DecidableType) : WSfun(E).
@@ -9,7 +9,127 @@ Module Foo (E : DecidableType) : WSfun(E).
   Definition elt := E.t.
   Definition t := Set_ elt.
   Definition In x s := member x s = true. 
+
+  Definition Equal s s' := forall a : elt, In a s <-> In a s'.
+  Definition Subset s s' := forall a : elt, In a s -> In a s'.
+  Definition Empty s := forall a : elt, ~ In a s.
+  Definition For_all (P : elt -> Prop) s := forall x, In x s -> P x.
+  Definition Exists (P : elt -> Prop) s := exists x, In x s /\ P x.
+
+
   Definition empty : t := empty.
-  
-  
+  Definition is_empty : t -> bool. Admitted.
+  Definition mem : elt -> t -> bool. Admitted.
+  Definition add : elt -> t -> t. Admitted.
+  Definition singleton : elt -> t. Admitted.
+  Definition remove : elt -> t -> t. Admitted.
+  Definition union : t -> t -> t. Admitted.
+  Definition inter : t -> t -> t. Admitted.
+  Definition diff : t -> t -> t. Admitted.
+  Definition eq : t -> t -> Prop := Equal.
+  Definition eq_dec : forall s s' : t, {eq s s'} + {~ eq s s'}. Admitted.
+  Definition equal : t -> t -> bool. Admitted.
+  Definition subset : t -> t -> bool. Admitted.
+  Definition fold : forall A : Type, (elt -> A -> A) -> t -> A -> A. Admitted.
+  Definition for_all : (elt -> bool) -> t -> bool. Admitted.
+  Definition exists_ : (elt -> bool) -> t -> bool. Admitted.
+  Definition filter : (elt -> bool) -> t -> t. Admitted.
+  Definition partition : (elt -> bool) -> t -> t * t. Admitted.
+  Definition cardinal : t -> nat. Admitted.
+  Definition elements : t -> list elt. Admitted.
+  Definition choose : t -> option elt. Admitted.
+  Lemma In_1 :
+    forall (s : t) (x y : elt), E.eq x y -> In x s -> In y s. Admitted.
+  Lemma eq_refl : forall s : t, eq s s. Admitted.
+  Lemma eq_sym : forall s s' : t, eq s s' -> eq s' s. Admitted.
+  Lemma eq_trans :
+    forall s s' s'' : t, eq s s' -> eq s' s'' -> eq s s''. Admitted.
+  Lemma mem_1 : forall (s : t) (x : elt), In x s -> mem x s = true. Admitted.
+  Lemma mem_2 : forall (s : t) (x : elt), mem x s = true -> In x s. Admitted.
+  Lemma equal_1 : forall s s' : t, Equal s s' -> equal s s' = true. Admitted.
+  Lemma equal_2 : forall s s' : t, equal s s' = true -> Equal s s'. Admitted.
+  Lemma subset_1 : forall s s' : t, Subset s s' -> subset s s' = true. Admitted.
+  Lemma subset_2 : forall s s' : t, subset s s' = true -> Subset s s'. Admitted.
+  Lemma empty_1 : Empty empty. Admitted.
+  Lemma is_empty_1 : forall s : t, Empty s -> is_empty s = true. Admitted.
+  Lemma is_empty_2 : forall s : t, is_empty s = true -> Empty s. Admitted.
+  Lemma add_1 :
+    forall (s : t) (x y : elt), E.eq x y -> In y (add x s). Admitted.
+  Lemma add_2 : forall (s : t) (x y : elt), In y s -> In y (add x s). Admitted.
+  Lemma add_3 :
+    forall (s : t) (x y : elt), ~ E.eq x y -> In y (add x s) -> In y s. Admitted.
+  Lemma remove_1 :
+    forall (s : t) (x y : elt), E.eq x y -> ~ In y (remove x s). Admitted.
+  Lemma remove_2 :
+    forall (s : t) (x y : elt), ~ E.eq x y -> In y s -> In y (remove x s). Admitted.
+  Lemma remove_3 :
+    forall (s : t) (x y : elt), In y (remove x s) -> In y s. Admitted.
+  Lemma singleton_1 :
+    forall x y : elt, In y (singleton x) -> E.eq x y. Admitted.
+  Lemma singleton_2 :
+    forall x y : elt, E.eq x y -> In y (singleton x). Admitted.
+  Lemma union_1 :
+    forall (s s' : t) (x : elt), In x (union s s') -> In x s \/ In x s'. Admitted.
+  Lemma union_2 :
+    forall (s s' : t) (x : elt), In x s -> In x (union s s'). Admitted.
+  Lemma union_3 :
+    forall (s s' : t) (x : elt), In x s' -> In x (union s s'). Admitted.
+  Lemma inter_1 :
+    forall (s s' : t) (x : elt), In x (inter s s') -> In x s. Admitted.
+  Lemma inter_2 :
+    forall (s s' : t) (x : elt), In x (inter s s') -> In x s'. Admitted.
+  Lemma inter_3 :
+    forall (s s' : t) (x : elt), In x s -> In x s' -> In x (inter s s'). Admitted.
+  Lemma diff_1 :
+    forall (s s' : t) (x : elt), In x (diff s s') -> In x s. Admitted.
+  Lemma diff_2 :
+    forall (s s' : t) (x : elt), In x (diff s s') -> ~ In x s'. Admitted.
+  Lemma diff_3 :
+    forall (s s' : t) (x : elt), In x s -> ~ In x s' -> In x (diff s s'). Admitted.
+  Lemma fold_1 :
+    forall (s : t) (A : Type) (i : A) (f : elt -> A -> A),
+    fold A f s i =
+    fold_left (fun (a : A) (e : elt) => f e a) (elements s) i. Admitted.
+  Lemma cardinal_1 : forall s : t, cardinal s = length (elements s). Admitted.
+  Lemma filter_1 :
+    forall (s : t) (x : elt) (f : elt -> bool),
+    compat_bool E.eq f -> In x (filter f s) -> In x s. Admitted.
+  Lemma filter_2 :
+    forall (s : t) (x : elt) (f : elt -> bool),
+    compat_bool E.eq f -> In x (filter f s) -> f x = true. Admitted.
+  Lemma filter_3 :
+    forall (s : t) (x : elt) (f : elt -> bool),
+    compat_bool E.eq f -> In x s -> f x = true -> In x (filter f s). Admitted.
+  Lemma for_all_1 :
+    forall (s : t) (f : elt -> bool),
+    compat_bool E.eq f ->
+    For_all (fun x : elt => f x = true) s -> for_all f s = true. Admitted.
+  Lemma for_all_2 :
+    forall (s : t) (f : elt -> bool),
+    compat_bool E.eq f ->
+    for_all f s = true -> For_all (fun x : elt => f x = true) s. Admitted.
+  Lemma exists_1 :
+    forall (s : t) (f : elt -> bool),
+    compat_bool E.eq f ->
+    Exists (fun x : elt => f x = true) s -> exists_ f s = true. Admitted.
+  Lemma exists_2 :
+    forall (s : t) (f : elt -> bool),
+    compat_bool E.eq f ->
+    exists_ f s = true -> Exists (fun x : elt => f x = true) s. Admitted.
+  Lemma partition_1 :
+    forall (s : t) (f : elt -> bool),
+    compat_bool E.eq f -> Equal (fst (partition f s)) (filter f s). Admitted.
+  Lemma partition_2 :
+    forall (s : t) (f : elt -> bool),
+    compat_bool E.eq f ->
+    Equal (snd (partition f s)) (filter (fun x : elt => negb (f x)) s). Admitted.
+  Lemma elements_1 :
+    forall (s : t) (x : elt), In x s -> InA E.eq x (elements s). Admitted.
+  Lemma elements_2 :
+    forall (s : t) (x : elt), InA E.eq x (elements s) -> In x s. Admitted.
+  Lemma elements_3w : forall s : t, NoDupA E.eq (elements s). Admitted.
+  Lemma choose_1 :
+    forall (s : t) (x : elt), choose s = Some x -> In x s. Admitted.
+  Lemma choose_2 : forall s : t, choose s = None -> Empty s. Admitted.
+
 End Foo.

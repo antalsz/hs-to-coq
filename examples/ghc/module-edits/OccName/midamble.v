6a6affda74bd79ba0726e702185243e5f6fe9f7e
Stephanie Weirich <sweirich@cis.upenn.edu>
2017-12-06 07:20:25 -0800

    lib/OccName.v

--- examples/ghc/module-edits/OccName/midamble.v
+++ examples/ghc/module-edits/OccName/midamble.v
@@ -42,3 +42,17 @@ Local Definition NameSpace_op_zlze__ : NameSpace -> NameSpace -> bool :=
             | Eq => true
             | _  => false
           end.
+
+(* Axioms for operations that we cannot yet translate *)
+
+Axiom isDataSymOcc : OccName -> bool.
+Axiom isSymOcc : OccName -> bool.
+Axiom chooseUniqueOcc : NameSpace -> GHC.Base.String -> OccSet -> OccName.
+Axiom startsWithUnderscore : OccName -> bool.
+Axiom isDerivedOccName : OccName -> bool.
+Axiom mkGenS : Module.Module -> OccName -> GHC.Num.Int -> GHC.Num.Int -> OccName.
+Axiom mkGenC : Module.Module -> OccName -> GHC.Num.Int -> OccName .
+Axiom tidyOccName : TidyOccEnv -> OccName -> (TidyOccEnv * OccName)%type.
+Axiom mkSuperDictAuxOcc : GHC.Num.Int -> OccName -> OccName.
+Axiom mkSuperDictSelOcc : GHC.Num.Int -> OccName -> OccName.
+Axiom mkLocalOcc : Unique.Unique -> OccName -> OccName.

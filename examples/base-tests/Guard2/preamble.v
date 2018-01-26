e49a37bc42a953d153d15bfb691cc453ac5a9b73
Stephanie Weirich <sweirich@cis.upenn.edu>
2018-01-25 20:10:59 -0500

    updating tests now that partial record selectors use GHC.Err.error. But there is no chance to add a Default instance. Ugh.

--- examples/base-tests/Guard2/preamble.v
+++ examples/base-tests/Guard2/preamble.v
@@ -1,24 +1,2 @@
-Require Export Coq.Lists.List.
-Axiom Char     : Type.
-Definition String := list Char.
-
-Require Coq.Strings.String.
-Require Coq.Strings.Ascii.
-
-Bind Scope string_scope with String.string.
-Bind Scope char_scope   with Ascii.ascii.
-
-Axiom hs_char__ : Ascii.ascii -> Char.
-
-Module GHC.
-Module Base.
-Fixpoint hs_string__ (s : String.string) : String :=
-  match s with
-  | String.EmptyString => nil
-  | String.String c s  => hs_char__ c :: hs_string__ s
-  end.
-End Base.
-End GHC.
-
-Axiom error : forall {A : Type}, String -> A.
-
+Require Import GHC.Base.
+Require Import GHC.Err.

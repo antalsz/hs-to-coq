29fec6f09fd61ff4a4185c05cbbe89ea2904e935
Christine Rizkallah <criz@seas.upenn.edu>
2018-02-08 18:44:31 -0500

    solve_bounded' tactic and a couple of lemmas proven using alternative bounded definition

--- examples/containers/theories/SetAlternativeDef.v
+++ examples/containers/theories/SetAlternativeDef.v
@@ -19,79 +19,43 @@ Set Bullet Behavior "Strict Subproofs".
 
 Local Open Scope Z_scope.
 
+Definition partial_lt {a} `{GHC.Base.Ord a}
+  (x : a) : a -> bool  :=
+  (fun arg => arg GHC.Base.< x).
+Definition partial_gt {a} `{GHC.Base.Ord a}
+           (x : a) : a -> bool :=
+  (fun arg => arg GHC.Base.> x).
 
 Module SM (E: OrderedType) : WSfun(E).
-  Include OrdTheories.OrdTheories E.
+Include OrdTheories.OrdTheories E.
 
-  Definition partial_lt (x : elt) : elt -> bool :=
-  (fun arg => arg GHC.Base.< x).
-  Definition partial_gt  (x : elt) : elt -> bool :=
-  (fun arg => arg GHC.Base.> x).
-  
-  Fixpoint bounded (lo hi: elt -> bool) (t': Set_ elt):=
-    match t' with
+Fixpoint bounded (lo hi: elt -> bool) (t': Set_ elt) : bool :=
+  match t' with
   | Tip => true
   | Bin _ x l r =>
     andb (lo x)
          (andb (hi x)
-                    (andb
-                       (bounded lo (partial_lt x) l)
-                       (bounded (partial_gt x) hi r)))
+               (andb
+                  (bounded lo (partial_lt x) l)
+                  (bounded (partial_gt x) hi r)))
   end.
 
-  Lemma partial_lt_mono : forall x y z,
-      E.lt y x \/ E.eq x y ->
-      partial_lt z x -> partial_lt z y.
-  Proof.
-    move=>x y z [Hlt | Heq]; rewrite /partial_lt;
-           autorewrite with elt_compare; intros; eauto.
-      apply E.eq_sym in Heq. eauto.
-  Qed.
-
-  Lemma partial_lt_relax : forall x y z,
+Lemma partial_lt_relax : forall x y z,
       E.lt x y \/ E.eq x y ->
       partial_lt x z -> partial_lt y z.
-  Proof.
-    move=>x y z [Hlt | Heq]; rewrite /partial_lt;
-           autorewrite with elt_compare; intros; eauto.
-  Qed.
+Proof.
+  move=>x y z [Hlt | Heq]; rewrite /partial_lt;
+         autorewrite with elt_compare; intros; eauto.
+Qed.
 
-  Lemma partial_gt_mono : forall x y z,
-      E.lt x y \/ E.eq x y ->
-      partial_gt z x -> partial_gt z y.
-  Proof.
-    move=>x y z [Hlt | Heq]; rewrite /partial_gt;
-           autorewrite with elt_compare; intros; eauto.
-  Qed.
-
-  Lemma partial_gt_relax : forall x y z,
-      E.lt y x \/ E.eq x y ->
-      partial_gt x z -> partial_gt y z.
-  Proof.
-    move=>x y z [Hlt | Heq]; rewrite /partial_gt;
-           autorewrite with elt_compare; intros; eauto.
+Lemma partial_gt_relax : forall x y z,
+    E.lt y x \/ E.eq x y ->
+    partial_gt x z -> partial_gt y z.
+Proof.
+  move=>x y z [Hlt | Heq]; rewrite /partial_gt;
+         autorewrite with elt_compare; intros; eauto.
     apply E.eq_sym in Heq; eauto.
-  Qed.
-
-
-(* Well-formedness alternative definition *)
-
-
-Fixpoint bounded' (t: Set_ elt) (lo hi: option elt): bool :=
-  match t with
-  | Tip => true
-  | Bin _ x l r =>
-    match lo with
-    | None => true
-    | Some lo' => (OrdFacts.lt_dec lo' x)
-    end &&
-     match hi with
-    | None => true
-    | Some hi' => (OrdFacts.lt_dec x hi')
-    end && 
-    bounded' l lo (Some x) && bounded' r (Some x) hi
-  end.
-
+Qed.
 
 Lemma partial_lt_eq: forall x y,
     partial_lt x y = OrdFacts.lt_dec y x.
@@ -125,6 +89,22 @@ Proof.
     auto.
 Qed.
 
+(* Well-formedness alternative definition *)
+
+Fixpoint bounded' (t: Set_ elt) (lo hi: option elt): bool :=
+  match t with
+  | Tip => true
+  | Bin _ x l r =>
+    match lo with
+    | None => true
+    | Some lo' => OrdFacts.lt_dec lo' x
+    end &&
+        match hi with
+    | None => true
+    | Some hi' => OrdFacts.lt_dec x hi'
+        end && 
+        bounded' l lo (Some x) && bounded' r (Some x) hi
+  end.
 
 Lemma andb_pw: forall a b a' b',
     a=a' -> b=b' ->
@@ -135,7 +115,7 @@ Proof.
   eauto.
 Qed.
 
-Lemma bounded_alt: forall t l r,
+Lemma bounded_alt: forall (t: Set_ elt) l r,
     let fl :=
         match l with
         | None => (fun _ => true)
@@ -167,7 +147,7 @@ Qed.
 
 Definition ordered' t := bounded' t None None.
 
-Lemma ordered_alt: forall t, ordered t = ordered' t.
+Lemma ordered_alt: forall t : Set_ elt, ordered  t = ordered' t.
 Proof.
   intros t.   
   rewrite /ordered'.
@@ -175,6 +155,139 @@ Proof.
   rewrite /ordered /bounded /partial_gt /partial_lt.
   reflexivity.
 Qed.
+
+Definition before_balancedL (x: elt) (l r : Set_ elt) : Prop :=
+    (size l + size r <= 2 /\ size r <= 1) \/
+    (size l <= delta * size r + 1 /\ size r <= delta * size l).
+
+Definition valid' `{Eq_ elt} `{Ord elt} (t : Set_ elt): bool :=
+  balanced t && ordered' t && validsize t.
+
+Definition WF (s : Set_ elt) := valid' s.
+
+Ltac solve_bounded' :=
+  repeat match goal with
+         | [H: is_true (bounded' _ _ _) |- _ ] =>
+           move: H; rewrite /bounded'=>?
+         | [ H: is_true (andb (andb (andb _ _) _) _) |- _ ] =>
+             let Hlo := fresh "Hlo" in
+             let Hhi := fresh "Hhi" in
+             let Hboundl := fresh "Hboundl" in
+             let Hboundh := fresh "Hboundh" in
+             move: H; repeat case /andP; 
+             move=>Hlo Hhi Hboundl Hboundh
+         | [ |- is_true(andb _ _) ] =>
+           apply /andP=>//; split=>//
+         end; autorewrite with elt_compare in *; eauto.
+
+(* Lemmas about bounded *)
+
+Lemma bounded_impl_left_None : forall t l r,
+      bounded' t l r -> bounded' t None r.
+Proof.
+  induction t; intros; auto.
+  solve_bounded'.
+Qed.
+
+Lemma bounded_impl_right_None : forall t l r,
+      bounded' t l r -> bounded' t l None.
+Proof.
+  induction t; intros; auto.
+  solve_bounded'.
+Qed.
+
+
+ Lemma balanceL_ordered : forall s (x: elt) (l r : Set_ elt) lo hi,
+      WF l ->
+      WF r ->
+      before_balancedL x l r ->
+      bounded' (Bin s x l r) lo hi  ->
+      bounded' (balanceL x l r) lo hi .
+  Proof.
+    move=> s x l r lo hi Hwfl Hwfr.
+    destruct r as [sr xr rl rr | ];
+      destruct l as [sl xl ll lr | ];
+      move=>Hbb Hb.
+    - 
+      destruct lo as [lo'|];
+        destruct hi as [hi'|].
+      + admit.
+      + admit.
+      + admit.
+      + move: Hb. rewrite /bounded'.
+        case /and2P.
+        do 6 rewrite andb_assoc.
+        case /and3P.
+        repeat apply andb_true_l.
+        repeat apply andb_true_r.
+        
+        Check andb_true_iff.
+        Check andb_true_l.
+        intros && H2]
+        repeat rewrite andb_true_l.
+        Check  eq_iff_eq_true.
+        repeat rewrite eq_iff_eq_true.
+        simpl.
+        match goal with
+           | [H: is_true (bounded' _ _ _) |- _ ] =>
+             move: H; rewrite /bounded'=>?
+         end.
+         
+           | [ H: is_true (andb _ (andb _ (andb _ ))) |- _ ] =>
+             let Hlo := fresh "Hlo" in
+             let Hhi := fresh "Hhi" in
+             let Hboundl := fresh "Hboundl" in
+             let Hboundh := fresh "Hboundh" in
+             move: H; case /and4P=>//; move=>Hlo Hhi Hboundl Hboundh
+           | [ |- is_true (andb _ (andb _ (andb _ _))) ] =>
+             apply /and4P=>//; split=>//
+           | [H: context[partial_lt _ _] |- _ ] =>
+             move: H; rewrite /partial_lt; move=>H
+           | [H: context[partial_gt _ _] |- _ ] =>
+             move: H; rewrite /partial_gt; move=>H
+           end. autorewrite with elt_compare in *; eauto.
+        unfold bounded' in *.
+        do 2 rewrite andb_true_r in Hb.
+        do 2 rewrite andb_true_l in Hb.
+        move : Hb.
+        case /and3P.
+        case /andP.
+        case /andP.
+        intros Hb1 Hb2 Hb3 Hb4 Hb5.
+        move : Hb4.
+        case /andP.
+        intros Hb4 Hb6.
+        rewrite /balanceL; rewrite_Int.
+        
+        destruct_match.
+        destruct ll as [sll xll lll llr | ];
+        destruct lr as [slr xlr lrl lrr | ].
+      + destruct_match.
+        * destruct lo as [lo'|].
+          dest
+          simpl in Hb.
+      auto.
+      try solve [solve_local_bounded].
+      destruct ll as [sll xll lll llr | ];
+        destruct lr as [slr xlr lrl lrr | ];
+        try solve [lucky_balanced_solve].
+          
+      rewrite /bounded' /balanceL; rewrite_Int.
+       
+               
+               try solve [solve_local_bounded].
+    - (** Both [r] and [l] are [Bin]s *)
+      destruct_match; try solve [solve_local_bounded].
+      destruct ll as [sll xll lll llr | ];
+        destruct lr as [slr xlr lrl lrr | ];
+        try solve [lucky_balanced_solve].
+      destruct_match; rewrite_Int; solve_local_bounded.
+    - (** [r] is a [Tip] *)
+      destruct ll as [sll xll lll llr | ];
+        destruct lr as [slr xlr lrl lrr | ];
+        try solve [solve_local_bounded].
+      destruct_match; solve_local_bounded.
+  Time Qed. (* Finished transaction in 0.67 secs (0.668u,0.001s) (successful) *)
 
 
  

277dda1802b2e3986fb0f41f2853ca5ce0f9fb4a
Joachim Breitner <mail@joachim-breitner.de>
2018-02-11 14:19:06 -0500

    Avoid local implicit arguments
    this works around #68.

--- base/Data/Foldable.v
+++ base/Data/Foldable.v
@@ -116,25 +116,24 @@ Definition default_elem {t : Type -> Type} {a} `{GHC.Base.Eq_ a} (foldMap : (a -
   a -> t a -> bool :=
    fun x xs => getAny (foldMap (fun y => Mk_Any (GHC.Base.op_zeze__ x y)) xs).
 
-
 Definition default_foldable {f:Type -> Type}
-  (foldMap : forall {m} {a}, forall `{GHC.Base.Monoid m}, (a -> m) -> f a -> m)
-  (foldr : forall {a} {b}, (a -> b -> b) -> b -> f a -> b):=
-  let foldl : forall {b} {a}, (b -> a -> b) -> b -> f a -> b :=
-      (fun {b} {a} =>
+  (foldMap : forall m a, forall (M : GHC.Base.Monoid m), (a -> m) -> f a -> m)
+  (foldr : forall a b, (a -> b -> b) -> b -> f a -> b):=
+  let foldl : forall b a, (b -> a -> b) -> b -> f a -> b :=
+      (fun b a =>
          fun f  z t => Data.Monoid.appEndo
                     (Data.Monoid.getDual
-                       (foldMap (Coq.Program.Basics.compose
+                       (foldMap _ _ _ (Coq.Program.Basics.compose
                                    Data.Monoid.Mk_Dual
                                    (Coq.Program.Basics.compose
                                       Data.Monoid.Mk_Endo
                                       (GHC.Base.flip f))) t)) z)
   in
-  let foldl' : forall {b} {a}, (b -> a -> b) -> b -> f a -> b :=
+  let foldl' : forall b a, (b -> a -> b) -> b -> f a -> b :=
       (fun {b} {a} =>
          fun f  z0  xs =>
            let f' :=  fun  x  k  z => GHC.Base.op_zdzn__ k (f z x)
-           in foldr f' GHC.Base.id xs z0)
+           in foldr _ _ f' GHC.Base.id xs z0)
   in
   Foldable__Dict_Build
     f
@@ -142,38 +141,38 @@ Definition default_foldable {f:Type -> Type}
        Coq.Program.Basics.compose
          (fun p => Coq.Program.Basics.compose
                   Data.Monoid.getAny
-                  (foldMap (fun x => Data.Monoid.Mk_Any (p x))))
+                  (foldMap _ _ _ (fun x => Data.Monoid.Mk_Any (p x))))
          GHC.Base.op_zeze__ )
     (* fold *)
-    (fun {m} `{GHC.Base.Monoid m} => foldMap GHC.Base.id)
+    (fun m (M : GHC.Base.Monoid m) => foldMap _ _ _ GHC.Base.id)
     (* foldMap *)
-    (fun {m}{a} `{GHC.Base.Monoid m} => foldMap)
+    (@foldMap)
     (* foldl *)
-    foldl
+    (@foldl)
     (* foldl' *)
-    foldl'
+    (@foldl')
     (* foldr  *)
-    (fun {a}{b} => foldr)
+    (@foldr)
     (* foldr' *)
-    (fun {a} {b} f z0 xs =>
+    (fun a b f z0 xs =>
        let f' := fun k  x  z => GHC.Base.op_zdzn__ k (f x z)
        in
-       foldl _ _ f' GHC.Base.id xs z0)
+       @foldl _ _ f' GHC.Base.id xs z0)
     (* length *)
-    (fun {a} => foldl' _ _ (fun c  _ => GHC.Num.op_zp__ c (GHC.Num.fromInteger 1))
+    (fun a => @foldl' _ a (fun c  _ => GHC.Num.op_zp__ c (GHC.Num.fromInteger 1))
                     (GHC.Num.fromInteger 0))
     (* null *)
-    (fun {a} => foldr (fun arg_61__ arg_62__ => false) true)
+    (fun a => @foldr _ _ (fun arg_61__ arg_62__ => false) true)
     (* product *)
-    (fun {a} `{GHC.Num.Num a} =>
+    (fun a `{GHC.Num.Num a} =>
        Coq.Program.Basics.compose Data.Monoid.getProduct
-                                  (foldMap Data.Monoid.Mk_Product))
+                                  (foldMap _ _ _ Data.Monoid.Mk_Product))
     (* sum *)
-    (fun  {a} `{GHC.Num.Num a} =>
+    (fun a `{GHC.Num.Num a} =>
        Coq.Program.Basics.compose Data.Monoid.getSum
-                                  (foldMap Data.Monoid.Mk_Sum))
+                                  (foldMap _ _ _ Data.Monoid.Mk_Sum))
     (* toList *)
-    (fun {a} => fun t => GHC.Base.build (fun c n => foldr c n t)).
+    (fun a => fun t => GHC.Base.build (fun c n => @foldr _ _ c n t)).
 
 Definition default_foldable_foldMap {f : Type -> Type}
   (foldMap : forall {m} {a}, forall `{GHC.Base.Monoid m}, (a -> m) -> f a -> m)

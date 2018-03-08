3da4fe97e4ce1ddd64ea920b136a4d2ca20a6635
Joachim Breitner <mail@joachim-breitner.de>
2018-03-08 10:37:14 -0500

    ptrEq, now working

--- examples/containers/manual/Utils/Containers/Internal/PtrEquality.v
+++ examples/containers/manual/Utils/Containers/Internal/PtrEquality.v
@@ -1,16 +1,16 @@
-.Lemma ptrEq_spec :
+Lemma ptrEq_spec :
   {ptrEq : forall a, a -> a -> bool | forall a (x:a)(y:a), ptrEq _ x y = true -> x = y}.
 Proof.  apply (exist _ (fun _ _ _ => false)). intros; congruence. Qed.
 
 Definition ptrEq : forall {a}, a -> a -> bool := proj1_sig ptrEq_spec.
 Lemma  ptrEq_eq : forall {a} (x:a)(y:a), ptrEq x y = true -> x = y.
-Proof. exact ptrEq_spec. Qed.
+Proof. exact (proj2_sig ptrEq_spec). Qed.
 
 Lemma hetPtrEq_spec :
-  {ptrEq : forall a b, a -> b -> bool
-  | forall a b (x:a)(y:a), ptrEq _ _ x y = true -> a = b /\ x = y}.
+  {hetPtrEq : forall a b, a -> b -> bool
+  | forall a b (x:a)(y:a), hetPtrEq _ _ x y = true -> a = b /\ x = y}.
 Proof.  apply (exist _ (fun _ _ _ _ => false)). intros; congruence. Qed.
 
 Definition hetPtrEq : forall {a b}, a -> b -> bool := proj1_sig hetPtrEq_spec.
-Lemma  hetPtrEq_eq :forall {a b} (x:a)(y:a), hetPtrEq  x y = true -> a = b /\ x = y.
-Proof. exact hetPtrEq_spec. Qed.
+Lemma  hetPtrEq_eq :forall {a b} (x:a)(y:a), hetPtrEq x y = true -> a = b /\ x = y.
+Proof. exact (proj2_sig hetPtrEq_spec). Qed.

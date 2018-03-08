3fcb9722f1ab47f60e7300cc86f8a9665732b4ec
Stephanie Weirich <sweirich@cis.upenn.edu>
2018-03-08 08:39:51 -0500

    trying to extract IntSet. Doesn't work yet.

---
+++ examples/containers/extraction/ExtractedIntSet.hs
@@ -0,0 +1,346 @@
+{-# LANGUAGE PatternSynonyms, ViewPatterns #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+-- This module is a wrapper (shim) for the extracted version of
+-- Data.IntSet.Internal
+
+
+module ExtractedIntSet where
+
+import qualified Base
+import qualified Datatypes
+import qualified BinNums
+
+import qualified Semigroup
+import qualified Monoid
+
+import qualified Internal0 as S2
+
+import qualified Data.Semigroup
+import qualified Data.Monoid
+import qualified Data.Foldable
+import qualified Data.Bits
+
+import qualified Control.Arrow as A
+import Control.DeepSeq(NFData,rnf)
+
+type IntSet = S2.IntSet
+pattern Nil = S2.Nil
+pattern Tip x y = S2.Tip x y
+pattern Bin x1 x2 x3 x4 = S2.Bin x1 x2 x3 x4
+
+type Mask   = S2.Mask
+type BitMap = S2.BitMap
+type Prefix = S2.Prefix
+type Key    = S2.Key
+
+instance Integral S2.Mask where
+  quot = undefined
+  rem  = undefined
+  div  = undefined
+  mod  = undefined
+  quotRem = undefined
+  divMod   = undefined
+  toInteger = undefined
+
+instance Eq S2.Prefix where
+  (==) = undefined
+
+instance Num S2.Prefix where
+  (+) = undefined
+  (-) = undefined
+  (*) = undefined
+  negate = undefined
+  abs = undefined
+  signum = undefined
+  fromInteger = undefined
+
+instance Data.Bits.Bits S2.Prefix where
+  (.&.) = undefined
+  (.|.) = undefined
+  xor   = undefined
+  complement = undefined
+  shift = undefined
+  rotate = undefined
+  bitSize = undefined
+  bitSizeMaybe = undefined
+  isSigned = undefined
+  testBit = undefined
+  bit = undefined
+  popCount = undefined
+
+
+
+----------------------------------------------------
+
+instance NFData (S2.IntSet) where
+    rnf (S2.Tip p b)     = p `seq` b `seq` ()
+    rnf (S2.Bin p y l r) = p `seq` y `seq` rnf l `seq` rnf r
+    rnf S2.Nil           = ()
+
+instance Real S2.Mask where
+  toRational = undefined
+
+instance Enum S2.Mask where
+  toEnum = undefined
+  fromEnum = undefined
+  
+instance Ord S2.Mask where
+  compare = undefined
+
+----------------------------------------------------
+
+instance Show BinNums.Coq_positive where
+  show bn = reverse (go bn) where
+    go BinNums.Coq_xH = "1"
+    go (BinNums.Coq_xI bn) = '1' : go bn
+    go (BinNums.Coq_xO bn) = 'O' : go bn
+  
+
+toPositive :: Int -> BinNums.Coq_positive
+toPositive x | x <= 0 = error "must call with positive int"
+toPositive 1 = BinNums.Coq_xH
+toPositive x = let b1 = x `mod` 2
+                   b2 = x `div` 2 in
+               if b1 == 1 then BinNums.Coq_xI (toPositive b2) else
+                               BinNums.Coq_xO (toPositive b2)
+
+fromPositive :: BinNums.Coq_positive -> Int
+fromPositive BinNums.Coq_xH = 1
+fromPositive (BinNums.Coq_xI bn) = fromPositive bn * 2 + 1
+fromPositive (BinNums.Coq_xO bn) = fromPositive bn * 2
+ 
+toBinZ :: Int -> BinNums.Z
+toBinZ 0 = BinNums.Z0
+toBinZ x | x < 0 = BinNums.Zneg (toPositive (abs x))
+toBinZ x | x > 0 = BinNums.Zpos (toPositive x)
+
+fromBinZ :: BinNums.Z -> Int
+fromBinZ BinNums.Z0 = 0
+fromBinZ (BinNums.Zneg bn) = - (fromPositive bn)
+fromBinZ (BinNums.Zpos bn) = fromPositive bn
+
+----------------------------------------------------
+
+instance Eq (S2.IntSet) where
+  (==) = S2.coq_Eq___IntSet_op_zeze__ 
+  (/=) = S2.coq_Eq___IntSet_op_zsze__ 
+
+instance Prelude.Ord (S2.IntSet) where
+  compare = S2.coq_Ord__IntSet_compare
+  (>)     = S2.coq_Ord__IntSet_op_zg__
+  (>=)    = S2.coq_Ord__IntSet_op_zgze__ 
+  (<)     = S2.coq_Ord__IntSet_op_zl__ 
+  (<=)    = S2.coq_Ord__IntSet_op_zlze__ 
+  max     = S2.coq_Ord__IntSet_max 
+  min     = S2.coq_Ord__IntSet_min 
+
+instance Data.Semigroup.Semigroup (S2.IntSet) where
+  (<>)    = S2.coq_Semigroup__IntSet_op_zlzg__ 
+  sconcat = error "no defn"
+  stimes  = error "no defn"
+
+instance Data.Monoid.Monoid (S2.IntSet) where
+  mempty  = S2.coq_Monoid__IntSet_mempty  
+  mappend = S2.coq_Monoid__IntSet_mappend 
+  mconcat = S2.coq_Monoid__IntSet_mconcat 
+
+instance Show (S2.IntSet) where
+  showsPrec p xs = showParen (p > 10) $
+    showString "fromList " . shows (toList xs)
+
+{-
+instance Data.Foldable.Foldable S2.IntSet where
+  fold    = fold
+  foldMap = S2.coq_Foldable__IntSet_foldMap monoid_a
+  foldr   = S2.foldr
+  foldr'  = S2.foldr'
+  foldl   = S2.foldl
+  foldl'  = S2.foldl'
+  foldr1  = error "foldr1: partial"
+  foldl1  = error "foldl1: partial"
+  toList  = S2.toList
+  null    = S2.null
+  length  = error "fix int problem" -- S2.length
+  elem    = S2.coq_Foldable__IntSet_elem eq_a
+  maximum = error "maximum: partial"
+  minimum = error "minimum: partial"
+  sum     = error "TODO, figure out Num shim"
+  product = error "TODO, figure out Num shim"
+-}
+
+zero :: Int -> Mask -> Bool
+zero x m = S2.zero (toBinZ x) m
+
+----------------------------------------------------------------
+-- for unit tests
+----------------------------------------------------------------
+
+lookupLT :: Int  -> IntSet -> Maybe Int
+lookupLT x s = fromBinZ <$> S2.lookupLT (toBinZ x) s
+
+lookupGT :: Int -> IntSet -> Maybe Int
+lookupGT x s = fromBinZ <$> S2.lookupGT  (toBinZ x) s
+
+lookupLE :: Int -> IntSet -> Maybe Int
+lookupLE x s = fromBinZ <$> S2.lookupLE  (toBinZ x) s
+
+lookupGE :: Int -> IntSet -> Maybe Int
+lookupGE x s = fromBinZ <$> S2.lookupGE  (toBinZ x) s
+
+
+--------------------------------------------------
+-- Indexed
+--------------------------------------------------
+
+findIndex   = error "findIndex: partial function"
+elemAt      = error "elemAt: partial function"
+deleteAt    = error "deleteAt: partial function"
+
+--------------------------------------------------
+-- Valid Trees
+--------------------------------------------------
+
+-- I dunno how to get pattern synonyms to do this
+bin s = S2.Bin (toBinZ s)
+
+-- need to translate BinNums.Z -> Int
+size :: IntSet -> Int
+size x = fromBinZ (S2.size x)
+
+--------------------------------------------------
+-- Single, Member, Insert, Delete
+--------------------------------------------------
+empty :: IntSet
+empty = S2.empty
+
+singleton :: Int -> IntSet
+singleton x = S2.singleton (toBinZ x)
+
+member ::  Int -> IntSet -> Bool
+member x = S2.member (toBinZ x)
+
+notMember ::  Int -> IntSet -> Bool
+notMember x = S2.notMember (toBinZ x)
+
+insert ::  Int -> IntSet -> IntSet
+insert x = S2.insert (toBinZ x)
+
+delete ::  Int -> IntSet -> IntSet
+delete x = S2.delete (toBinZ x)
+
+{--------------------------------------------------------------------
+  Balance
+--------------------------------------------------------------------}
+
+split ::  Int -> IntSet -> (IntSet, IntSet)
+split x = S2.split (toBinZ x)
+
+link = S2.link
+
+{--------------------------------------------------------------------
+  Union
+--------------------------------------------------------------------}
+
+union :: (IntSet) -> (IntSet) -> IntSet
+union = S2.union 
+
+difference :: (IntSet) -> (IntSet) -> IntSet
+difference = S2.difference 
+
+intersection ::  (IntSet) -> (IntSet) -> IntSet
+intersection = S2.intersection
+
+disjoint ::  (IntSet) -> (IntSet) -> Bool
+disjoint = S2.disjoint
+
+null :: IntSet -> Bool
+null = S2.null
+
+{--------------------------------------------------------------------
+  Lists
+--------------------------------------------------------------------}
+
+fromAscList :: [Int] -> IntSet
+fromAscList xs = error "untranslated"
+
+fromDistinctAscList :: [Int] -> IntSet
+fromDistinctAscList xs = error "untranslated"
+
+
+fromList :: [Int] -> IntSet
+fromList xs = S2.fromList (Prelude.map toBinZ xs)
+
+
+toDescList :: IntSet -> [Int]
+toDescList x = Prelude.map fromBinZ (S2.toDescList x)
+
+toAscList :: IntSet -> [Int]
+toAscList x = Prelude.map fromBinZ (S2.toAscList x)
+
+toList :: IntSet -> [Int]
+toList x = Prelude.map fromBinZ (S2.toList x)
+
+
+{--------------------------------------------------------------------
+  Set operations are like IntSet operations
+--------------------------------------------------------------------}
+
+
+foldr :: (Int -> a -> a) -> a -> IntSet -> a
+foldr f b s = S2.foldr (f . fromBinZ) b s
+
+foldr' :: (Int -> a -> a) -> a -> IntSet -> a
+foldr' f b s = S2.foldr' (f . fromBinZ) b s
+
+foldl' :: (a -> Int -> a) -> a -> IntSet -> a
+foldl' f b s = S2.foldl' (\ x y -> f x (fromBinZ y)) b s
+
+foldl :: (a -> Int -> a) -> a -> IntSet -> a
+foldl f b s = S2.foldl (\x y -> f x (fromBinZ y)) b s
+
+fold   = S2.fold
+filter = S2.filter
+map    = S2.map
+
+
+elems  :: IntSet -> [Int]
+elems x = Prelude.map fromBinZ (S2.elems x) 
+
+isProperSubsetOf :: IntSet -> IntSet -> Bool
+isProperSubsetOf = S2.isProperSubsetOf 
+
+isSubsetOf :: IntSet -> IntSet -> Bool
+isSubsetOf = S2.isSubsetOf 
+
+findMax :: IntSet -> Int
+findMax = error "partial"
+
+findMin :: IntSet -> Int
+findMin = error "partial"
+
+
+minView :: IntSet -> Maybe (Int,IntSet)
+minView s = (A.first fromBinZ) <$> S2.minView s
+
+maxView :: IntSet -> Maybe (Int,IntSet)
+maxView s = (A.first fromBinZ) <$> S2.maxView s
+
+splitMember :: Int -> IntSet -> (IntSet, Bool, IntSet)
+splitMember m s = (x,y,z) where
+  ((x,y),z) = S2.splitMember (toBinZ m) s
+
+
+unions :: [IntSet] -> IntSet
+unions = S2.unions 
+
+splitRoot :: IntSet -> [IntSet]
+splitRoot = S2.splitRoot
+
+partition :: (Int -> Bool) -> IntSet -> (IntSet, IntSet)
+partition f = S2.partition (f . fromBinZ)
+
+match :: Int -> S2.Prefix -> S2.Mask -> Prelude.Bool
+match x y z = S2.match_ (toBinZ x) y z
+
+
--- examples/containers/extraction/src/Extract.v
+++ examples/containers/extraction/src/Extract.v
@@ -12,13 +12,13 @@ Require Coq.Program.Wf.
 
 Require GHC.Base.
 Require GHC.Enum.
-Require Import Data.Set.Internal.
+Require Data.Either.
+Require Utils.Containers.Internal.PtrEquality.
+
 Require Import mathcomp.ssreflect.ssreflect.
 
 Extraction Blacklist Prelude.
-Extraction Blacklist Internal.
 Extraction Language Haskell.
-
 
 Set Warnings "-extraction-reserved-identifier".
 
@@ -29,14 +29,33 @@ Extract Inductive option => "Prelude.Maybe" [ "Prelude.Just" "Prelude.Nothing"].
 Extract Inductive prod => "(,)" ["(,)"].
 Extract Inductive Either.Either => "Prelude.Either" [ "Prelude.Left" "Prelude.Right" ].
 
+Require Import Data.Set.Internal.
+
 Extract Constant patternFailure => "GHC.Base.undefined".
 Extract Constant PtrEquality.ptrEq => "\ x y -> Prelude.False".
 Extract Constant PtrEquality.hetPtrEq => "\ x y -> Prelude.False".
 Extract Constant Base.errorWithoutStackTrace => "errorWithoutStackTrace".
 Extract Constant unsafeFix => "(\f -> let r = f r in r)".
 
-Require Import Data.Set.Internal.
+(*
+-- I'm trying to convert Z to Int, but this does not work. 
+Require Import GHC.Num.
+Extract Constant Num.Int => "Prelude.Int".
+Extract Constant Num.Num_Int__ => "Prelude.undefined".
+Require GHC.Char.
+Extract Constant Char.Char => "Prelude.Char".
+Extract Constant Char.hs_char__ => "Prelude.undefined".
+Extract Constant Char.chr => "Prelude.undefined".
+Extract Constant Char.ord => "Prelude.undefined".
+Require Import GHC.Base.
+Extract Constant Base.Eq_Int___ => "Prelude.undefined".
+Extract Constant Base.Ord_Int___ => "Prelude.undefined".
+Extract Constant Base.Eq_Char___ => "Prelude.undefined".
+Extract Constant Base.Ord_Char___ => "Prelude.undefined". *)
+
 Recursive Extraction Library Internal.
+
+Extraction Blacklist Internal.
 
 Require Import Data.IntSet.Internal.
 Recursive Extraction Library Internal.

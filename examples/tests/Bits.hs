a544f73bc7d453d65b595c29b885a3925247b2ea
Joachim Breitner <mail@joachim-breitner.de>
2018-03-09 16:25:43 -0500

    No tests with deferredFix in tests

--- examples/tests/Bits.hs
+++ examples/tests/Bits.hs
@@ -17,6 +17,7 @@ testBitDefault = \x i -> (x .&. i)
 testBitDefault2 :: Bits a => a -> a -> a
 testBitDefault2 = (.&.)
 
+{-
 popCountDefault :: (Bits a) =>
     a -> (a -> a) -> (a -> a) -> (a -> Bool) -> a -> a
 popCountDefault zero inc dec isZero = go zero
@@ -24,3 +25,4 @@ popCountDefault zero inc dec isZero = go zero
    go c w = if isZero w
             then c
             else go (inc c) (w .&. (dec w))
+-}

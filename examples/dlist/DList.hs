4a6d3fd1124be435a73d4937182276fb2fd1267c
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2018-03-21 01:41:44 -0400

    Maybe `DList`s are well-formed if they could have been `fromList`

--- examples/dlist/DList.hs
+++ examples/dlist/DList.hs
@@ -9,17 +9,20 @@ where
 
 data DList a = DList { fromDList :: [a] -> [a] }
 
+fromList :: [a] -> DList a
+fromList = DList . (++)
+
+toList :: DList a -> [a]
+toList (DList f) = f []
+
 empty :: DList a
 empty = DList id
 
 singleton :: a -> DList a
 singleton a = DList (a:)
 
-append :: DList a -> DList a -> DList a
-append (DList a) (DList b) = DList (a . b)
-
 cons :: a -> DList a -> DList a
 cons a (DList b) = DList $ (a:) . b
 
-toList :: DList a -> [a]
-toList (DList f) = f []
+append :: DList a -> DList a -> DList a
+append (DList a) (DList b) = DList (a . b)

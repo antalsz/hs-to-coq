9dc0d0bc1e50364d6d628c2c69eba9a9c00bf6f2
Joachim Breitner <mail@joachim-breitner.de>
2018-03-22 14:50:26 -0400

    Translate more of InstructionI

--- examples/riscv/lib/ExecuteI.v
+++ examples/riscv/lib/ExecuteI.v
@@ -15,7 +15,6 @@ Require Coq.Program.Wf.
 Require Data.Bits.
 Require Decode.
 Require GHC.Base.
-Require GHC.Err.
 Require GHC.Num.
 Require GHC.Real.
 Require Monads.
@@ -132,50 +131,50 @@ Definition execute {p} {t} `{(Program.RiscvProgram p t)}
                                                                                              else Program.setPC addr))))
     | Decode.Lb rd rs1 oimm12 =>
         Monads.Bind (Program.getRegister rs1) (fun a =>
-                       Monads.Bind (VirtualMemory.translate VirtualMemory.Load 1 (a GHC.Num.+
-                                                             Utility.fromImm oimm12)) (fun addr =>
+                       Monads.Bind (Program.translate Program.Load 1 (a GHC.Num.+
+                                                       Utility.fromImm oimm12)) (fun addr =>
                                       Monads.Bind (Program.loadByte addr) (fun x =>
                                                      Program.setRegister rd (Utility.int8ToReg x))))
     | Decode.Lh rd rs1 oimm12 =>
         Monads.Bind (Program.getRegister rs1) (fun a =>
-                       Monads.Bind (VirtualMemory.translate VirtualMemory.Load 2 (a GHC.Num.+
-                                                             Utility.fromImm oimm12)) (fun addr =>
+                       Monads.Bind (Program.translate Program.Load 2 (a GHC.Num.+
+                                                       Utility.fromImm oimm12)) (fun addr =>
                                       Monads.Bind (Program.loadHalf addr) (fun x =>
                                                      Program.setRegister rd (Utility.int16ToReg x))))
     | Decode.Lw rd rs1 oimm12 =>
         Monads.Bind (Program.getRegister rs1) (fun a =>
-                       Monads.Bind (VirtualMemory.translate VirtualMemory.Load 4 (a GHC.Num.+
-                                                             Utility.fromImm oimm12)) (fun addr =>
+                       Monads.Bind (Program.translate Program.Load 4 (a GHC.Num.+
+                                                       Utility.fromImm oimm12)) (fun addr =>
                                       Monads.Bind (Program.loadWord addr) (fun x =>
                                                      Program.setRegister rd (Utility.int32ToReg x))))
     | Decode.Lbu rd rs1 oimm12 =>
         Monads.Bind (Program.getRegister rs1) (fun a =>
-                       Monads.Bind (VirtualMemory.translate VirtualMemory.Load 1 (a GHC.Num.+
-                                                             Utility.fromImm oimm12)) (fun addr =>
+                       Monads.Bind (Program.translate Program.Load 1 (a GHC.Num.+
+                                                       Utility.fromImm oimm12)) (fun addr =>
                                       Monads.Bind (Program.loadByte addr) (fun x =>
                                                      Program.setRegister rd (Utility.uInt8ToReg x))))
     | Decode.Lhu rd rs1 oimm12 =>
         Monads.Bind (Program.getRegister rs1) (fun a =>
-                       Monads.Bind (VirtualMemory.translate VirtualMemory.Load 2 (a GHC.Num.+
-                                                             Utility.fromImm oimm12)) (fun addr =>
+                       Monads.Bind (Program.translate Program.Load 2 (a GHC.Num.+
+                                                       Utility.fromImm oimm12)) (fun addr =>
                                       Monads.Bind (Program.loadHalf addr) (fun x =>
                                                      Program.setRegister rd (Utility.uInt16ToReg x))))
     | Decode.Sb rs1 rs2 simm12 =>
         Monads.Bind (Program.getRegister rs1) (fun a =>
-                       Monads.Bind (VirtualMemory.translate VirtualMemory.Store 1 (a GHC.Num.+
-                                                             Utility.fromImm simm12)) (fun addr =>
+                       Monads.Bind (Program.translate Program.Store 1 (a GHC.Num.+
+                                                       Utility.fromImm simm12)) (fun addr =>
                                       Monads.Bind (Program.getRegister rs2) (fun x =>
                                                      Program.storeByte addr (Utility.regToInt8 x))))
     | Decode.Sh rs1 rs2 simm12 =>
         Monads.Bind (Program.getRegister rs1) (fun a =>
-                       Monads.Bind (VirtualMemory.translate VirtualMemory.Store 2 (a GHC.Num.+
-                                                             Utility.fromImm simm12)) (fun addr =>
+                       Monads.Bind (Program.translate Program.Store 2 (a GHC.Num.+
+                                                       Utility.fromImm simm12)) (fun addr =>
                                       Monads.Bind (Program.getRegister rs2) (fun x =>
                                                      Program.storeHalf addr (Utility.regToInt16 x))))
     | Decode.Sw rs1 rs2 simm12 =>
         Monads.Bind (Program.getRegister rs1) (fun a =>
-                       Monads.Bind (VirtualMemory.translate VirtualMemory.Store 4 (a GHC.Num.+
-                                                             Utility.fromImm simm12)) (fun addr =>
+                       Monads.Bind (Program.translate Program.Store 4 (a GHC.Num.+
+                                                       Utility.fromImm simm12)) (fun addr =>
                                       Monads.Bind (Program.getRegister rs2) (fun x =>
                                                      Program.storeWord addr (Utility.regToInt32 x))))
     | Decode.Addi rd rs1 imm12 =>
@@ -249,11 +248,11 @@ Definition execute {p} {t} `{(Program.RiscvProgram p t)}
         Monads.Bind (Program.getRegister rs1) (fun x =>
                        Monads.Bind (Program.getRegister rs2) (fun y =>
                                       Program.setRegister rd (x Data.Bits..&.(**) y)))
-    | inst => GHC.Err.error "dispatch bug: "
+    | inst => Monads.Return tt
     end.
 
 (* Unbound variables:
-     bool negb unit Coq.ZArith.BinInt.Z.eqb Coq.ZArith.BinInt.Z.lor
+     bool negb tt unit Coq.ZArith.BinInt.Z.eqb Coq.ZArith.BinInt.Z.lor
      Data.Bits.complement Data.Bits.op_zizazi__ Data.Bits.xor Decode.Add Decode.Addi
      Decode.And Decode.Andi Decode.Auipc Decode.Beq Decode.Bge Decode.Bgeu Decode.Blt
      Decode.Bltu Decode.Bne Decode.InstructionI Decode.Jal Decode.Jalr Decode.Lb
@@ -261,13 +260,12 @@ Definition execute {p} {t} `{(Program.RiscvProgram p t)}
      Decode.Sb Decode.Sh Decode.Sll Decode.Slli Decode.Slt Decode.Slti Decode.Sltiu
      Decode.Sltu Decode.Sra Decode.Srai Decode.Srl Decode.Srli Decode.Sub Decode.Sw
      Decode.Xor Decode.Xori GHC.Base.op_zgze__ GHC.Base.op_zl__ GHC.Base.op_zsze__
-     GHC.Base.when GHC.Err.error GHC.Num.op_zm__ GHC.Num.op_zp__ GHC.Real.mod_
-     Monads.Bind Program.RiscvProgram Program.getPC Program.getRegister
-     Program.loadByte Program.loadHalf Program.loadWord Program.raiseException
-     Program.setPC Program.setRegister Program.storeByte Program.storeHalf
-     Program.storeWord Utility.fromImm Utility.int16ToReg Utility.int32ToReg
-     Utility.int8ToReg Utility.ltu Utility.regToInt16 Utility.regToInt32
-     Utility.regToInt8 Utility.regToShamt Utility.sll Utility.sra Utility.srl
-     Utility.uInt16ToReg Utility.uInt8ToReg VirtualMemory.Load VirtualMemory.Store
-     VirtualMemory.translate
+     GHC.Base.when GHC.Num.op_zm__ GHC.Num.op_zp__ GHC.Real.mod_ Monads.Bind
+     Monads.Return Program.Load Program.RiscvProgram Program.Store Program.getPC
+     Program.getRegister Program.loadByte Program.loadHalf Program.loadWord
+     Program.raiseException Program.setPC Program.setRegister Program.storeByte
+     Program.storeHalf Program.storeWord Program.translate Utility.fromImm
+     Utility.int16ToReg Utility.int32ToReg Utility.int8ToReg Utility.ltu
+     Utility.regToInt16 Utility.regToInt32 Utility.regToInt8 Utility.regToShamt
+     Utility.sll Utility.sra Utility.srl Utility.uInt16ToReg Utility.uInt8ToReg
 *)

c9901302a23d79fba296ac1211659716b2ed9347
Joachim Breitner <mail@joachim-breitner.de>
2018-04-03 14:47:11 -0400

    Mark more modules using CoreType as broken

--- examples/ghc/lib/CmmType.v
+++ examples/ghc/lib/CmmType.v
@@ -59,10 +59,6 @@ Instance Default__CmmCat : GHC.Err.Default CmmCat :=
   GHC.Err.Build_Default _ GcPtrCat.
 (* Midamble *)
 
-Instance Default__CmmCat  : GHC.Err.Default CmmCat :=
-	 { default := GcPtrCat }.
-Instance Default__width   : GHC.Err.Default Width :=
-	 { default := W80 }.
 Instance Default__CmmType : GHC.Err.Default CmmType :=
 	 { default := Mk_CmmType GHC.Err.default GHC.Err.default }.
 
--- examples/ghc/module-edits/CmmType/midamble.v
+++ examples/ghc/module-edits/CmmType/midamble.v
@@ -1,6 +1,2 @@
-Instance Default__CmmCat  : GHC.Err.Default CmmCat :=
-	 { default := GcPtrCat }.
-Instance Default__width   : GHC.Err.Default Width :=
-	 { default := W80 }.
 Instance Default__CmmType : GHC.Err.Default CmmType :=
 	 { default := Mk_CmmType GHC.Err.default GHC.Err.default }.

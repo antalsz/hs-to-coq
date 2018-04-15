1151ed4324a95639f54a8532d8c6280333cadd88
Stephanie Weirich <sweirich@cis.upenn.edu>
2018-04-15 09:08:15 -0400

    use mapAccumL in Bag. Finish CoreSubst. Placeholder for invariants about substitution.

--- examples/ghc/lib/Bag.v
+++ examples/ghc/lib/Bag.v
@@ -13,10 +13,12 @@ Require Coq.Program.Wf.
 (* Converted imports: *)
 
 Require Control.Monad.
+Require Coq.Program.Basics.
 Require Data.Either.
 Require Data.Foldable.
 Require Data.Maybe.
 Require Data.OldList.
+Require Data.SemigroupInternal.
 Require Data.Traversable.
 Require GHC.Base.
 Require GHC.List.
@@ -46,8 +48,6 @@ Arguments ListBag {_} _.
 (* Skipping instance Outputable__Bag of class Outputable *)
 
 (* Skipping instance Data__Bag of class Data *)
-
-(* Skipping instance Foldable__Bag *)
 
 Definition allBag {a} : (a -> bool) -> Bag a -> bool :=
   fix allBag arg_0__ arg_1__
@@ -132,6 +132,82 @@ Definition foldrBag {a} {r} : (a -> r -> r) -> r -> Bag a -> r :=
 Definition bagToList {a} : Bag a -> list a :=
   fun b => foldrBag cons nil b.
 
+Local Definition Foldable__Bag_foldr
+   : forall {a} {b}, (a -> b -> b) -> b -> Bag a -> b :=
+  fun {a} {b} => foldrBag.
+
+Local Definition Foldable__Bag_null : forall {a}, Bag a -> bool :=
+  fun {a} => Foldable__Bag_foldr (fun arg_0__ arg_1__ => false) true.
+
+Local Definition Foldable__Bag_toList : forall {a}, Bag a -> list a :=
+  fun {a} =>
+    fun t => GHC.Base.build' (fun _ => (fun c n => Foldable__Bag_foldr c n t)).
+
+Local Definition Foldable__Bag_foldl'
+   : forall {b} {a}, (b -> a -> b) -> b -> Bag a -> b :=
+  fun {b} {a} =>
+    fun f z0 xs =>
+      let f' := fun x k z => k (f z x) in Foldable__Bag_foldr f' GHC.Base.id xs z0.
+
+Local Definition Foldable__Bag_length : forall {a}, Bag a -> GHC.Num.Int :=
+  fun {a} =>
+    Foldable__Bag_foldl' (fun arg_0__ arg_1__ =>
+                            match arg_0__, arg_1__ with
+                            | c, _ => c GHC.Num.+ #1
+                            end) #0.
+
+Local Definition Foldable__Bag_foldMap
+   : forall {m} {a}, forall `{GHC.Base.Monoid m}, (a -> m) -> Bag a -> m :=
+  fun {m} {a} `{GHC.Base.Monoid m} =>
+    fun f => Foldable__Bag_foldr (GHC.Base.mappend GHC.Base.∘ f) GHC.Base.mempty.
+
+Local Definition Foldable__Bag_foldl
+   : forall {b} {a}, (b -> a -> b) -> b -> Bag a -> b :=
+  fun {b} {a} =>
+    fun f z t =>
+      Data.SemigroupInternal.appEndo (Data.SemigroupInternal.getDual
+                                      (Foldable__Bag_foldMap (Data.SemigroupInternal.Mk_Dual GHC.Base.∘
+                                                              (Data.SemigroupInternal.Mk_Endo GHC.Base.∘
+                                                               GHC.Base.flip f)) t)) z.
+
+Local Definition Foldable__Bag_foldr'
+   : forall {a} {b}, (a -> b -> b) -> b -> Bag a -> b :=
+  fun {a} {b} =>
+    fun f z0 xs =>
+      let f' := fun k x z => k (f x z) in Foldable__Bag_foldl f' GHC.Base.id xs z0.
+
+Local Definition Foldable__Bag_product
+   : forall {a}, forall `{GHC.Num.Num a}, Bag a -> a :=
+  fun {a} `{GHC.Num.Num a} =>
+    Coq.Program.Basics.compose Data.SemigroupInternal.getProduct
+                               (Foldable__Bag_foldMap Data.SemigroupInternal.Mk_Product).
+
+Local Definition Foldable__Bag_sum
+   : forall {a}, forall `{GHC.Num.Num a}, Bag a -> a :=
+  fun {a} `{GHC.Num.Num a} =>
+    Coq.Program.Basics.compose Data.SemigroupInternal.getSum (Foldable__Bag_foldMap
+                                Data.SemigroupInternal.Mk_Sum).
+
+Local Definition Foldable__Bag_fold
+   : forall {m}, forall `{GHC.Base.Monoid m}, Bag m -> m :=
+  fun {m} `{GHC.Base.Monoid m} => Foldable__Bag_foldMap GHC.Base.id.
+
+Program Instance Foldable__Bag : Data.Foldable.Foldable Bag :=
+  fun _ k =>
+    k {| Data.Foldable.fold__ := fun {m} `{GHC.Base.Monoid m} =>
+           Foldable__Bag_fold ;
+         Data.Foldable.foldMap__ := fun {m} {a} `{GHC.Base.Monoid m} =>
+           Foldable__Bag_foldMap ;
+         Data.Foldable.foldl__ := fun {b} {a} => Foldable__Bag_foldl ;
+         Data.Foldable.foldl'__ := fun {b} {a} => Foldable__Bag_foldl' ;
+         Data.Foldable.foldr__ := fun {a} {b} => Foldable__Bag_foldr ;
+         Data.Foldable.foldr'__ := fun {a} {b} => Foldable__Bag_foldr' ;
+         Data.Foldable.length__ := fun {a} => Foldable__Bag_length ;
+         Data.Foldable.null__ := fun {a} => Foldable__Bag_null ;
+         Data.Foldable.product__ := fun {a} `{GHC.Num.Num a} => Foldable__Bag_product ;
+         Data.Foldable.sum__ := fun {a} `{GHC.Num.Num a} => Foldable__Bag_sum ;
+         Data.Foldable.toList__ := fun {a} => Foldable__Bag_toList |}.
+
 Definition foldrBagM {m} {a} {b} `{(GHC.Base.Monad m)}
    : (a -> b -> m b) -> b -> Bag a -> m b :=
   fix foldrBagM arg_0__ arg_1__ arg_2__
@@ -166,6 +242,21 @@ Definition lengthBag {a} : Bag a -> GHC.Num.Int :=
 
 Definition listToBag {a} : list a -> Bag a :=
   fun arg_0__ => match arg_0__ with | nil => EmptyBag | vs => ListBag vs end.
+
+Definition mapAccumBagL {acc} {x} {y}
+   : (acc -> x -> (acc * y)%type) -> acc -> Bag x -> (acc * Bag y)%type :=
+  fix mapAccumBagL arg_0__ arg_1__ arg_2__
+        := match arg_0__, arg_1__, arg_2__ with
+           | _, s, EmptyBag => pair s EmptyBag
+           | f, s, UnitBag x => let 'pair s1 x1 := f s x in pair s1 (UnitBag x1)
+           | f, s, TwoBags b1 b2 =>
+               let 'pair s1 b1' := mapAccumBagL f s b1 in
+               let 'pair s2 b2' := mapAccumBagL f s1 b2 in
+               pair s2 (TwoBags b1' b2')
+           | f, s, ListBag xs =>
+               let 'pair s' xs' := Data.Traversable.mapAccumL f s xs in
+               pair s' (ListBag xs')
+           end.
 
 Definition mapAccumBagLM {m} {acc} {x} {y} `{GHC.Base.Monad m}
    : (acc -> x -> m (acc * y)%type) -> acc -> Bag x -> m (acc * Bag y)%type :=
@@ -427,13 +518,24 @@ Definition catBagMaybes {a} : Bag (option a) -> Bag a :=
 
 (* External variables:
      None Some andb bool cons false list nil op_zt__ option orb pair true tt unit
-     Control.Monad.filterM Data.Either.Either Data.Either.Left Data.Either.Right
-     Data.Foldable.all Data.Foldable.any Data.Foldable.foldl Data.Foldable.foldr
-     Data.Foldable.length Data.Foldable.mapM_ Data.Maybe.mapMaybe
-     Data.OldList.partition Data.Traversable.mapM GHC.Base.Eq_ GHC.Base.Functor
-     GHC.Base.Monad GHC.Base.const GHC.Base.fmap__ GHC.Base.map GHC.Base.op_z2218U__
-     GHC.Base.op_zeze__ GHC.Base.op_zgzg__ GHC.Base.op_zgzgze__ GHC.Base.op_zlzd____
-     GHC.Base.return_ GHC.List.filter GHC.List.unzip GHC.Num.Int GHC.Num.fromInteger
-     GHC.Num.op_zp__ MonadUtils.anyM MonadUtils.foldlM MonadUtils.foldrM
-     MonadUtils.mapAccumLM Util.isSingleton Util.partitionWith
+     Control.Monad.filterM Coq.Program.Basics.compose Data.Either.Either
+     Data.Either.Left Data.Either.Right Data.Foldable.Foldable Data.Foldable.all
+     Data.Foldable.any Data.Foldable.foldMap__ Data.Foldable.fold__
+     Data.Foldable.foldl Data.Foldable.foldl'__ Data.Foldable.foldl__
+     Data.Foldable.foldr Data.Foldable.foldr'__ Data.Foldable.foldr__
+     Data.Foldable.length Data.Foldable.length__ Data.Foldable.mapM_
+     Data.Foldable.null__ Data.Foldable.product__ Data.Foldable.sum__
+     Data.Foldable.toList__ Data.Maybe.mapMaybe Data.OldList.partition
+     Data.SemigroupInternal.Mk_Dual Data.SemigroupInternal.Mk_Endo
+     Data.SemigroupInternal.Mk_Product Data.SemigroupInternal.Mk_Sum
+     Data.SemigroupInternal.appEndo Data.SemigroupInternal.getDual
+     Data.SemigroupInternal.getProduct Data.SemigroupInternal.getSum
+     Data.Traversable.mapAccumL Data.Traversable.mapM GHC.Base.Eq_ GHC.Base.Functor
+     GHC.Base.Monad GHC.Base.Monoid GHC.Base.build' GHC.Base.const GHC.Base.flip
+     GHC.Base.fmap__ GHC.Base.id GHC.Base.map GHC.Base.mappend GHC.Base.mempty
+     GHC.Base.op_z2218U__ GHC.Base.op_zeze__ GHC.Base.op_zgzg__ GHC.Base.op_zgzgze__
+     GHC.Base.op_zlzd____ GHC.Base.return_ GHC.List.filter GHC.List.unzip GHC.Num.Int
+     GHC.Num.Num GHC.Num.fromInteger GHC.Num.op_zp__ MonadUtils.anyM
+     MonadUtils.foldlM MonadUtils.foldrM MonadUtils.mapAccumLM Util.isSingleton
+     Util.partitionWith
 *)
---
+++ examples/ghc/theories/CoreSubstInvariants.v
@@ -0,0 +1,106 @@
+Require Import CoreSyn.
+Require Import Id.
+Require Import IdInfo.
+Require Import VarSet.
+Require Import BasicTypes.
+
+Require Import Coq.Lists.List.
+Require Import Coq.Bool.Bool.
+Require Import Coq.ZArith.BinInt.
+
+Set Bullet Behavior "Strict Subproofs".
+
+(*
+-- | A substitution environment, containing 'Id', 'TyVar', and 'CoVar'
+-- substitutions.
+--
+-- Some invariants apply to how you use the substitution:
+--
+-- 1. #in_scope_invariant# The in-scope set contains at least those 'Id's and 'TyVar's that will be in scope /after/
+-- applying the substitution to a term. Precisely, the in-scope set must be a superset of the free vars of the
+-- substitution range that might possibly clash with locally-bound variables in the thing being substituted in.
+--
+-- 2. #apply_once# You may apply the substitution only /once/
+--
+-- There are various ways of setting up the in-scope set such that the first of these invariants hold:
+--
+-- * Arrange that the in-scope set really is all the things in scope
+--
+-- * Arrange that it's the free vars of the range of the substitution
+--
+-- * Make it empty, if you know that all the free vars of the substitution are fresh, and hence can't possibly clash
+data Subst
+  = Subst InScopeSet  -- Variables in in scope (both Ids and TyVars) /after/
+                      -- applying the substitution
+          IdSubstEnv  -- Substitution from NcIds to CoreExprs
+          TvSubstEnv  -- Substitution from TyVars to Types
+          CvSubstEnv  -- Substitution from CoVars to Coercions
+
+        -- INVARIANT 1: See #in_scope_invariant#
+        -- This is what lets us deal with name capture properly
+        -- It's a hard invariant to check...
+        --
+        -- INVARIANT 2: The substitution is apply-once; see Note [Apply once] with
+        --              Types.TvSubstEnv
+        --
+        -- INVARIANT 3: See Note [Extending the Subst]
+
+{-
+Note [Extending the Subst]
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+For a core Subst, which binds Ids as well, we make a different choice for Ids
+than we do for TyVars.
+
+For TyVars, see Note [Extending the TCvSubst] with Type.TvSubstEnv
+
+For Ids, we have a different invariant
+        The IdSubstEnv is extended *only* when the Unique on an Id changes
+        Otherwise, we just extend the InScopeSet
+
+In consequence:
+
+* If all subst envs are empty, substExpr would be a
+  no-op, so substExprSC ("short cut") does nothing.
+
+  However, substExpr still goes ahead and substitutes.  Reason: we may
+  want to replace existing Ids with new ones from the in-scope set, to
+  avoid space leaks.
+
+* In substIdBndr, we extend the IdSubstEnv only when the unique changes
+
+* If the CvSubstEnv, TvSubstEnv and IdSubstEnv are all empty,
+  substExpr does nothing (Note that the above rule for substIdBndr
+  maintains this property.  If the incoming envts are both empty, then
+  substituting the type and IdInfo can't change anything.)
+
+* In lookupIdSubst, we *must* look up the Id in the in-scope set, because
+  it may contain non-trivial changes.  Example:
+        (/\a. \x:a. ...x...) Int
+  We extend the TvSubstEnv with [a |-> Int]; but x's unique does not change
+  so we only extend the in-scope set.  Then we must look up in the in-scope
+  set when we find the occurrence of x.
+
+* The requirement to look up the Id in the in-scope set means that we
+  must NOT take no-op short cut when the IdSubst is empty.
+  We must still look up every Id in the in-scope set.
+
+* (However, we don't need to do so for expressions found in the IdSubst
+  itself, whose range is assumed to be correct wrt the in-scope set.)
+
+Why do we make a different choice for the IdSubstEnv than the
+TvSubstEnv and CvSubstEnv?
+
+* For Ids, we change the IdInfo all the time (e.g. deleting the
+  unfolding), and adding it back later, so using the TyVar convention
+  would entail extending the substitution almost all the time
+
+* The simplifier wants to look up in the in-scope set anyway, in case it
+  can see a better unfolding from an enclosing case expression
+
+* For TyVars, only coercion variables can possibly change, and they are
+  easy to spot
+-}
+
+
+
+*)

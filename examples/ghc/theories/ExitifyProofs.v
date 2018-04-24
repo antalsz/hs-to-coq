20d1f6a59a9b718818971b028865be3366447ec2
Joachim Breitner <mail@joachim-breitner.de>
2018-04-24 15:53:44 -0400

    Outline how a proof about `go` would look like
    if one is careful about not doing zeta-reduction (local definitions),
    more of the Haskell code structure remains and one can actually prove
    case by step. The `j_10__` definitions are actually useful here!
    
    This proof will need a better story about termination. We probably need
    a height function for `CoreExpr`.
    
    This is actually a proof about a monadic function with a state monad,
    where I defined some combinators to proof that a monadic function is
    preserving some invariant on the state. Not to bad, at least when the
    invariant is not changing ... if it were more complicated we’d probably
    have to reinvent hoare logic here.

--- examples/ghc/theories/ExitifyProofs.v
+++ examples/ghc/theories/ExitifyProofs.v
@@ -20,6 +20,21 @@ Set Bullet Behavior "Strict Subproofs".
 
 Require Import JoinPointInvariants.
 
+(** Tactics *)
+
+Ltac expand_pairs :=
+match goal with
+  |- context[let (_,_) := ?e in _] =>
+  rewrite (surjective_pairing e)
+end.
+
+Ltac simpl_bool :=
+  rewrite ?orb_true_l, ?orb_true_r, ?orb_false_l, ?orb_false_r,
+          ?andb_true_l, ?andb_true_r, ?andb_false_l, ?andb_false_r,
+          ?orb_true_iff, ?andb_true_iff
+          in *.
+
+
 
 (** ** Punted-on lemmas about GHC functions *)
 
@@ -41,6 +56,55 @@ Axiom forallb_imp:
   (forall x, P x = true -> Q x = true) ->
   forallb Q xs = true.
 
+Definition StateInvariant {a} {s} (P : s -> Prop) (act : State.State s a) :=
+  forall s, P s -> P (snd (State.runState act s)).
+
+Lemma StateInvariant_return:
+  forall {a s} (P : s -> Prop) (x : a),
+  StateInvariant P (return_ x).
+Proof.
+  intros.
+  intros s0 H. apply H.
+Qed.
+
+Lemma StateInvariant_bind:
+  forall {a b s} P (act1 : State.State s a) (act2 : a -> State.State s b),
+  StateInvariant P act1 ->
+  (forall x, StateInvariant P (act2 x)) ->
+  StateInvariant P (act1 >>= act2).
+Proof.
+  intros ?????? H1 H2.
+  intros s0 H.
+  unfold State.runState.
+  expand_pairs; simpl.
+  expand_pairs; simpl.
+  enough (P (snd (State.runState (act2 (fst (State.runState act1 s0))) (snd (State.runState act1 s0))))) by admit.
+  eapply H2.
+  eapply H1.
+  assumption.
+Admitted.
+
+Lemma StateInvariant_bind_return: (*  acommon pattern *)
+  forall {a b s} P (act1 : State.State s a) (f : a -> b),
+  StateInvariant P act1 ->
+  StateInvariant P (act1 >>= (fun x => return_ (f x))).
+Proof.
+  intros.
+  apply StateInvariant_bind.
+  * apply H.
+  * intro. apply StateInvariant_return.
+Qed.
+
+Lemma StateInvariant_forM:
+  forall {a b s} P (act : a -> State.State s b) (xs : list a),
+  (forall x, In x xs -> StateInvariant P (act x)) ->
+  StateInvariant P (Traversable.forM xs act).
+Proof.
+  intros ?????? Hact.
+  induction xs.
+  * intros s0 H. apply H.
+  * admit.
+Admitted.
 
 (* This section reflects the context of the local definition of exitify *)
 Section in_exitify.
@@ -88,19 +152,6 @@ Section in_exitify.
       let def := constr:(rhs) in (* Aha, we only have to abstract over the actually captured variables here. *)
       exact def
     end).
-
-  Ltac expand_pairs :=
-  match goal with
-    |- context[let (_,_) := ?e in _] =>
-    rewrite (surjective_pairing e)
-  end.
-
-  Ltac simpl_bool :=
-    rewrite ?orb_true_l, ?orb_true_r, ?orb_false_l, ?orb_false_r,
-            ?andb_true_l, ?andb_true_r, ?andb_false_l, ?andb_false_r,
-            ?orb_true_iff, ?andb_true_iff
-            in *.
-
   (** When is the result of [mkExitLets] valid? *)
   
   Lemma mkExitLets_JPI:
@@ -126,6 +177,149 @@ Section in_exitify.
           eapply forallb_imp. apply Hpairs.
           intros [v' rhs'].
           apply isJoinPointValidPair_extendVarSet.
+  Qed.
+
+  Axiom unsafe_deferredFix2_eq: forall {a b c} `{GHC.Err.Default c} (f : (a -> b -> c) -> (a -> b -> c)),
+    forall x y, DeferredFix.deferredFix2 f x y = f (DeferredFix.deferredFix2 f) x y.
+
+  Definition go_f := ltac:(
+    let rhs := eval cbv delta [go] in go in
+    lazymatch rhs with
+      | let x := recursive_calls in  @DeferredFix.deferredFix2 ?a ?b ?r ?HDefault ?f =>
+      let f' := constr:(let x := recursive_calls in f) in
+      exact f'
+    end).
+    
+  Lemma go_eq : forall x y, go x y = go_f go x y.
+  Proof. apply unsafe_deferredFix2_eq. Qed.
+
+  Lemma addExit_all_joinIds:
+    forall in_scope_set ty ja e,
+    StateInvariant (fun xs => forallb (fun '(v0, _) => isJoinId v0) xs = true)
+                   (addExit in_scope_set ty ja e).
+  Admitted.
+
+  Lemma go_all_joinIds:
+    forall captured ann_e,
+    StateInvariant (fun xs => forallb (fun '(v0, _) => isJoinId v0) xs = true)
+                   (go captured ann_e).
+  Proof.
+    set (P := (fun xs => forallb (fun '(v0, _) => isJoinId v0) xs = true)).
+    (* This cannot be structural recursion. Will need a size on expression. *)
+    fix 2. rename go_all_joinIds into IH.
+    intros.
+    rewrite go_eq.
+    cbv delta [go_f]. (* No [zeta]! *)
+    (* Float out lets *)
+    repeat lazymatch goal with | |- StateInvariant ?P ((let x := ?rhs in ?body) ?a ?b ?c) =>
+      change (let x := rhs in StateInvariant P (body a b c)); intro
+    end.
+    cbv beta. (* Do some beta reduction *)
+    (* Flout out more lets *)
+    repeat lazymatch goal with | |- StateInvariant ?P ((let x := ?rhs in ?body)) =>
+      change (let x := rhs in StateInvariant P body); intro
+    end.
+
+    (* First case *)
+    enough (Hnext: StateInvariant P j_37__). {
+      destruct (collectArgs e) as [rhs fun_args] eqn:HcA.
+      destruct rhs; try apply Hnext.
+      destruct (isJoinId i && Foldable.all isCapturedVarArg fun_args) ; try apply Hnext.
+      apply StateInvariant_return.
+    }
+
+    (* Second case *)
+    subst j_37__.
+    enough (Hnext: StateInvariant P j_36__). {
+      destruct (is_exit && negb is_interesting) ; try apply Hnext.
+      apply StateInvariant_return.
+    }
+
+    (* Third case *)
+    subst j_36__.
+    enough (Hnext: StateInvariant P j_35__). {
+      destruct (is_exit && captures_join_points ) ; try apply Hnext.
+      apply StateInvariant_return.
+    }
+
+    (* Third case: Actual exitification *)
+    subst j_35__.
+    enough (Hnext: StateInvariant P j_22__). {
+      destruct is_exit ; try apply Hnext.
+      apply StateInvariant_bind_return.
+      apply addExit_all_joinIds.
+    }
+    clear is_exit isCapturedVarArg is_interesting captures_join_points args e fvs.
+
+    (* Fourth case: No exitification here, so descend*)
+    subst j_22__.
+    enough (Hnext: StateInvariant P j_4__). {
+      destruct ann_e as [fvs e] eqn:Hann.
+      destruct e; try apply Hnext.
+      * (* Case [Case] *)
+        apply StateInvariant_bind_return.
+        apply StateInvariant_forM.
+        intros [[dc pats] rhs] HIn.
+        apply StateInvariant_bind_return.
+        apply IH.
+      * (* Case Let *) 
+        (* Flout out more lets *)
+        repeat lazymatch goal with | |- StateInvariant ?P ((let x := ?rhs in ?body)) =>
+          change (let x := rhs in StateInvariant P body); intro
+        end.
+
+        enough (Hnext': StateInvariant P j_18__). {
+          destruct a as [j rhs|pairs']; try apply Hnext'.
+          destruct (isJoinId_maybe j) as [join_arity|] eqn:HiJI; try apply Hnext'.
+          destruct (collectNAnnBndrs join_arity rhs) as [params join_body].
+          apply StateInvariant_bind.
+          + apply IH.
+          + intros. apply StateInvariant_bind_return.
+            apply IH.
+        }
+
+        subst j_18__.
+        enough (Hnext': StateInvariant P j_10__). {
+          destruct a as [j rhs|pairs']; try apply Hnext'.
+          destruct (isJoinId (Tuple.fst (Panic.head pairs'))); try apply Hnext'.
+          + intros.
+            apply StateInvariant_bind.
+            - apply StateInvariant_forM.
+              intros [j rhs] HIn.
+              cbv zeta.
+              destruct (collectNAnnBndrs (idJoinArity j) rhs) as [params join_body].
+              apply StateInvariant_bind_return.
+              apply IH.
+            - intro x.
+              apply StateInvariant_bind_return.
+              apply IH.
+        }
+
+        subst j_10__.
+        apply StateInvariant_bind_return.
+        apply IH.
+    }
+
+    subst j_4__.
+    apply StateInvariant_return.
+
+  (* Not structurally recursive *)
+  Fail Guarded.
+  Admitted.
+
+  Lemma all_exists_joinIds:
+    forallb (fun '(v, _) => isJoinId v) exits = true.
+  Proof.
+    unfold exits.
+    unfold pairs'_exits.
+    apply StateInvariant_forM.
+    * intros [v rhs] HIn.
+      expand_pairs.
+      apply StateInvariant_bind.
+      + apply go_all_joinIds.
+      + intro x.
+        apply StateInvariant_return.
+    * reflexivity.
   Qed.
 
   (** Main result *)

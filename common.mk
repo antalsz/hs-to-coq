14fe1cfeabaeba04102bc63186d468c46df2a83f
Joachim Breitner <mail@joachim-breitner.de>
2018-05-28 12:30:16 +0200

    Allow for use of system-installed hs-to-coq
    (for the artifact)

--- common.mk
+++ common.mk
@@ -1,6 +1,8 @@
-
 HAVE_STACK := $(shell command -v stack 2> /dev/null)
-ifdef HAVE_STACK
+
+ifeq ($(USE_SYSTEM_HS_TO_COQ),"True")
+HS_TO_COQ = HS_TO_COQ
+else ifdef HAVE_STACK
 HS_TO_COQ = stack exec hs-to-coq --
 else
 ifeq ($(HS_TO_COQ_COVERAGE),True)

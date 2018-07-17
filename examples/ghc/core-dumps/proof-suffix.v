457ce0a9b1d37549a8c37d60204f07f9c9351752
Joachim Breitner <mail@joachim-breitner.de>
2018-07-17 15:07:00 -0400

    Also prove `isJoinPointsValidProgram program` about dumped core ASTs.

--- examples/ghc/core-dumps/proof-suffix.v
+++ examples/ghc/core-dumps/proof-suffix.v
@@ -1,6 +1,12 @@
 Require Import Proofs.ScopeInvariant.
 Goal WellScopedProgram program.
+Proof.
 repeat (lazy; constructor).
 (* This is for the NoDup goals: *)
 all: rewrite ?IntSetProofs.In_cons_iff; intuition congruence.
+Qed.
+Require Import Proofs.JoinPointInvariants.
+Goal isJoinPointsValidProgram program.
+Proof.
+repeat (lazy; constructor).
 Qed.

4e9bea73188c0cda96e551573f7f69d6c0417d7c
Joachim Breitner <mail@joachim-breitner.de>
2018-08-10 14:34:19 -0400

    Make the cabal-based setup more robust
    to allow out-of-tree projects that follow the `examples/…` lead

--- common.mk
+++ common.mk
@@ -1,12 +1,14 @@
-
 HAVE_STACK := $(shell command -v stack 2> /dev/null)
+
 ifdef HAVE_STACK
 HS_TO_COQ = stack exec hs-to-coq --
 else
 ifeq ($(HS_TO_COQ_COVERAGE),True)
 	CABAL_OPTS = --enable-coverage
 endif
-HS_TO_COQ = cabal new-run -v0 $(CABAL_OPTS) exe:hs-to-coq --
+TOP := $(dir $(lastword $(MAKEFILE_LIST)))
+HS_TO_COQ = cabal new-run --project-file=$(TOP)/cabal.project  -v0 $(CABAL_OPTS) exe:hs-to-coq --
 endif
+
 SHELL = bash
 

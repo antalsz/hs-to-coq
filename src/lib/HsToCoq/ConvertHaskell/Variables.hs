8556295b992001e5ed677e53a1065b8bf2c390fc
Joachim Breitner <mail@joachim-breitner.de>
2018-08-10 16:07:16 -0400

    `exists` is also a keyword

--- src/lib/HsToCoq/ConvertHaskell/Variables.hs
+++ src/lib/HsToCoq/ConvertHaskell/Variables.hs
@@ -49,7 +49,7 @@ escapeReservedNames :: Ident -> Ident
 escapeReservedNames x =
   fromMaybe x . getFirst $
     foldMap (First . flip tryEscapeReservedWord x)
-            (T.words "Set Type Prop fun fix forall return mod match as cons pair nil for is with left right")
+            (T.words "Set Type Prop fun fix forall return mod match as cons pair nil for is with left right exists")
     <> if | T.all (== '.') x  -> pure $ T.map (const '∘') x
           | T.all (== '∘') x  -> pure $ "⟨" <> x <> "⟩"
 -- these type operators aren't parsed by the renaming file

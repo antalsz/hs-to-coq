3057396c36acadb42afb2c556581306f2c1e6e95
Joachim Breitner <mail@joachim-breitner.de>
2018-08-10 15:48:17 -0400

    Also translate Data.Map.Strict
    for the benefit of https://github.com/rsnikhil/RISCV-ISA-Spec

---
+++ examples/containers/lib/Data/Map/Strict.v
@@ -0,0 +1,16 @@
+(* Default settings (from HsToCoq.Coq.Preamble) *)
+
+Generalizable All Variables.
+
+Unset Implicit Arguments.
+Set Maximal Implicit Insertion.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Require Coq.Program.Tactics.
+Require Coq.Program.Wf.
+
+(* No imports to convert. *)
+
+(* No type declarations to convert. *)
+(* No value declarations to convert. *)

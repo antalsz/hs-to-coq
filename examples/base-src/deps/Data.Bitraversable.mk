64cee4c5890409c44f5458114285f8120ccac9c3
Stephanie Weirich <sweirich@cis.upenn.edu>
2018-09-06 10:43:53 -0400

    updated Coq

--- examples/base-src/deps/Data.Bitraversable.mk
+++ examples/base-src/deps/Data.Bitraversable.mk
@@ -1,1 +1,1 @@
-../../base/Data/Bitraversable.v: ../../base/Data/Either.h2ci ../../base/Data/Functor/Const.h2ci
+../../base/Data/Bitraversable.v: ../../base/Data/Bifoldable.h2ci ../../base/Data/Bifunctor.h2ci ../../base/Data/Either.h2ci ../../base/Data/Functor/Const.h2ci

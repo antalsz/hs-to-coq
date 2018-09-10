aab8e4549aeea3463958fa134d8ac94e5fbbb7aa
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2018-09-10 12:50:54 -0400

    Add support for uses of `common.mk` outside of this directory

--- common.mk
+++ common.mk
@@ -1,8 +1,18 @@
 HAVE_STACK := $(shell command -v stack 2> /dev/null)
 
 ifdef HAVE_STACK
-HS_TO_COQ = stack exec hs-to-coq --
+ifdef FOREIGN_HS_TO_COQ
+ifdef HS_TO_COQ_DIR
+$(error "Using `hs-to-coq/common.mk' outside the hs-to-coq directory requires setting `HS_TO_COQ_DIR'")
+endif
+HS_TO_COQ = $(shell cd $(HS_TO_COQ_DIR) && stack exec env | perl -ne 'print "$$1/hs-to-coq\n" if /^PATH=([^:]+)/')
 else
+HS_TO_COQ = stack exec hs-to-coq --
+endif
+else
+ifdef FOREIGN_HS_TO_COQ
+$(error "Using `hs-to-coq/common.mk' outside the hs-to-coq directory requires `stack'")
+endif
 ifeq ($(HS_TO_COQ_COVERAGE),True)
 	CABAL_OPTS = --enable-coverage
 endif
@@ -11,4 +21,3 @@ HS_TO_COQ = cabal new-run --project-file=$(TOP)/cabal.project  -v0 $(CABAL_OPTS)
 endif
 
 SHELL = bash
-

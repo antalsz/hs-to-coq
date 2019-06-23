f7ef984fafd2a7bacf5a1a99d66425b620be717c
Stephanie Weirich <sweirich@cis.upenn.edu>
2019-06-23 16:00:13 -0400

    translated CoreStats module

--- examples/ghc/lib/Core.v
+++ examples/ghc/lib/Core.v
@@ -2240,44 +2240,38 @@ Require GHC.Err.
 
 (*****)
 
-Instance Default_RuleInfo : GHC.Err.Default RuleInfo :=
+Instance Default__RuleInfo : GHC.Err.Default RuleInfo :=
   GHC.Err.Build_Default _ EmptyRuleInfo.
 
-Instance Default_Unfolding : GHC.Err.Default Unfolding :=
+Instance Default__Unfolding : GHC.Err.Default Unfolding :=
   GHC.Err.Build_Default _ NoUnfolding.
 
-Instance Default_TickBoxOp : GHC.Err.Default TickBoxOp :=
+Instance Default__TickBoxOp : GHC.Err.Default TickBoxOp :=
   GHC.Err.Build_Default _ (TickBox GHC.Err.default GHC.Err.default).
 
-Instance Default_CafInfo : GHC.Err.Default CafInfo :=
-  GHC.Err.Build_Default _ MayHaveCafRefs.
-
-Instance Default_Termination {r} : GHC.Err.Default (Termination r) :=
+Instance Default__Termination {r} : GHC.Err.Default (Termination r) :=
   GHC.Err.Build_Default _ Diverges.
 
-Instance Default_DmdType : GHC.Err.Default DmdType :=
+Instance Default__DmdType : GHC.Err.Default DmdType :=
   GHC.Err.Build_Default _ (Mk_DmdType GHC.Err.default GHC.Err.default GHC.Err.default).
 
-Instance Default_StrictSig : GHC.Err.Default StrictSig :=
+Instance Default__StrictSig : GHC.Err.Default StrictSig :=
   GHC.Err.Build_Default _ (Mk_StrictSig GHC.Err.default).
 
-Instance Default_JointDmd `{GHC.Err.Default a} `{GHC.Err.Default b} : GHC.Err.Default (JointDmd a b) :=
+Instance Default__JointDmd `{GHC.Err.Default a} `{GHC.Err.Default b} : GHC.Err.Default (JointDmd a b) :=
   GHC.Err.Build_Default _ (JD GHC.Err.default GHC.Err.default).
 
-Instance Default_Str {s} : GHC.Err.Default (Str s) :=
+Instance Default__Str {s} : GHC.Err.Default (Str s) :=
   GHC.Err.Build_Default _ Lazy.
-Instance Default_Use {s} : GHC.Err.Default (Use s) :=
+Instance Default__Use {s} : GHC.Err.Default (Use s) :=
   GHC.Err.Build_Default _ Abs.
 
-Instance Default_IdInfo : GHC.Err.Default IdInfo :=
+Instance Default__IdInfo : GHC.Err.Default IdInfo :=
   GHC.Err.Build_Default _ (Mk_IdInfo GHC.Err.default GHC.Err.default GHC.Err.default
                          GHC.Err.default GHC.Err.default GHC.Err.default GHC.Err.default
                          GHC.Err.default GHC.Err.default GHC.Err.default GHC.Err.default).
 
-Instance Default_TyCon : GHC.Err.Default TyCon :=
-  GHC.Err.Build_Default _ (PrimTyCon GHC.Err.default GHC.Err.default nil tt tt GHC.Err.default GHC.Err.default GHC.Err.default GHC.Err.default ).
-
-Instance Default_RecSelParent : GHC.Err.Default RecSelParent :=
+Instance Default__RecSelParent : GHC.Err.Default RecSelParent :=
   GHC.Err.Build_Default _ (RecSelData GHC.Err.default).
 
 Instance Default__Var : GHC.Err.Default Var := GHC.Err.Build_Default _ (Mk_Id GHC.Err.default GHC.Err.default GHC.Err.default GHC.Err.default GHC.Err.default GHC.Err.default).

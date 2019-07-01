b4ca311950a8e6d414e702e4e6084642f344ba61
Stephanie Weirich <sweirich@cis.upenn.edu>
2019-07-01 13:27:43 -0400

    missing file.

--- examples/ghc/lib/Name.v
+++ examples/ghc/lib/Name.v
@@ -10,10 +10,6 @@ Unset Printing Implicit Defensive.
 Require Coq.Program.Tactics.
 Require Coq.Program.Wf.
 
-(* Preamble *)
-
-Axiom TyThing : Type.
-
 (* Converted imports: *)
 
 Require AxiomatizedTypes.

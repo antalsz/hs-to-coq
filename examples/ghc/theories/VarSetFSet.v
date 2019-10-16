a2fbd11302d14f993f054c5e9903a3988b576c34
Yao Li <hnkfliyao@gmail.com>
2019-10-16 19:08:38 -0400

    VarSetFSet. WIP.

--- examples/ghc/theories/VarSetFSet.v
+++ examples/ghc/theories/VarSetFSet.v
@@ -24,7 +24,7 @@ Require Import Proofs.GHC.Base.
 Require Import Proofs.GHC.List.
 
 (* containers theory *)
-Require Import IntSetProofs. 
+Require Import IntSetProofs.
 
 (* ghc theory (incl. some that should move above. *)
 Require Import Proofs.Base.
@@ -41,10 +41,10 @@ Set Bullet Behavior "Strict Subproofs".
 
 (** ** [VarSet as FSet] *)
 
-(* This part creates an instance of the FSetInterface for VarSets. 
+(* This part creates an instance of the FSetInterface for VarSets.
 
-   This allows us to experiment with some of the metalib automation 
-   for reasoning about sets of variable names. 
+   This allows us to experiment with some of the metalib automation
+   for reasoning about sets of variable names.
 
    This file use the FSet instance to define modules of facts about VarSets
    including:
@@ -53,7 +53,7 @@ Set Bullet Behavior "Strict Subproofs".
      VarSetProperties
      VarSetDecide     [fsetdec tactic]
      VarSetNotin      [destruct_notin and solve_notin tactics]
-  
+
 
    *)
 
@@ -139,14 +139,14 @@ Module VarSetFSet <: WSfun(Var_as_DT) <: WS.
 
   Lemma In_1 :
     forall (s : t) (x y : elt), E.eq x y -> In x s -> In y s.
-  Proof. 
+  Proof.
     unfold E.eq, E.eqb, In.
     move => [u].
     move: u => [i].
     move=> x y Eq.
     unfold elemVarSet, UniqSet.elementOfUniqSet, UniqFM.elemUFM.
     erewrite member_eq with (k' := (Unique.getWordKey (Unique.getUnique y))). auto.
-    rewrite -> eq_unique in Eq. 
+    rewrite -> eq_unique in Eq.
     rewrite Eq.
     reflexivity.
   Qed.
@@ -155,7 +155,7 @@ Module VarSetFSet <: WSfun(Var_as_DT) <: WS.
   Proof. destruct s. unfold eq. unfold Equal. intro. reflexivity. Qed.
 
   Lemma eq_sym : forall s s' : t, eq s s' -> eq s' s.
-  Proof. destruct s; destruct s'; 
+  Proof. destruct s; destruct s';
     unfold eq, Equal in *. intros. rewrite H. intuition. Qed.
 
   Lemma eq_trans :
@@ -165,14 +165,43 @@ Module VarSetFSet <: WSfun(Var_as_DT) <: WS.
     unfold eq, Equal. intros ???. rewrite H. rewrite H0. reflexivity.
   Qed.
 
+  Require Import MapProofs.
+
   Lemma subset_1 : forall s s' : t, Subset s s' -> subset s s' = true.
   Proof.
     move => [[i]] [[i']].
     unfold subset,Subset in *.
     unfold In in *.
     unfold subVarSet, minusVarSet, isEmptyVarSet.
-    simpl. intros.
-  Admitted.
+    destruct i; destruct i'. simpl.
+    unfold IntMap.difference, IntMap.null, IntMap.member. simpl.
+    intros. eapply difference_Desc; try eassumption.
+    unfold diffo'. intros.
+    assert (forall i, sem s i = None).
+    { intros. specialize (H3 i).
+      specialize (H (Mk_Id GHC.Err.default
+                           i
+                           GHC.Err.default
+                           GHC.Err.default
+                           GHC.Err.default
+                           GHC.Err.default)).
+      simpl in H.
+      destruct (sem x0 i) eqn:Hsem0.
+      - assumption.
+      - destruct (sem x i) eqn:Hsem.
+        + assert (exists v, sem x i = Some v) by (eapply ex_intro; apply Hsem).
+          rewrite <- member_spec in H4; [|eassumption].
+          apply H in H4. rewrite -> member_spec in H4; [|eassumption].
+          destruct H4. rewrite Hsem0 in H4. inversion H4.
+        + assumption.
+    }
+    destruct s.
+    - simpl in H4. specialize (H4 w1).
+      rewrite Eq_refl in H4. simpl in H4.
+      rewrite oro_assoc oro_Some_l in H4.
+      destruct (sem s2 w1); simpl in H4; inversion H4.
+    - reflexivity.
+  Qed.
 
   Lemma subset_2 : forall s s' : t, subset s s' = true -> Subset s s'.
   Proof.
@@ -181,8 +210,19 @@ Module VarSetFSet <: WSfun(Var_as_DT) <: WS.
     unfold subset,Subset in *.
     unfold In in *.
     unfold subVarSet, minusVarSet, isEmptyVarSet.
-    simpl.
-  Admitted.
+    destruct j, j'. simpl.
+    unfold IntMap.null, IntMap.difference, IntMap.member. simpl.
+    eapply difference_Desc; eauto.
+    intros. rewrite -> null_spec' in H3; [|assumption].
+    rewrite member_spec; [|eassumption].
+    rewrite -> member_spec in H4; [|eassumption].
+    specialize (H2 (realUnique a)).
+    specialize (H3 (realUnique a)).
+    rewrite H3 in H2. destruct H4. rewrite H4 in H2.
+    destruct (sem x0 (realUnique a)) eqn:Hx0.
+    - exists v. assumption.
+    - simpl in H2. inversion H2.
+  Qed.
 
   Lemma empty_1 : Empty empty.
   Proof. unfold Empty; intros a H. inversion H. Qed.
@@ -190,66 +230,158 @@ Module VarSetFSet <: WSfun(Var_as_DT) <: WS.
   Lemma is_empty_1 : forall s : t, Empty s -> is_empty s = true.
   Proof.
     move=>[[i]]. unfold Empty, In. simpl.
-    move=> h.
-  Admitted.
+    rewrite /IntMap.member /IntMap.null.
+    move=> h. destruct i. simpl.
+    destruct x.
+    - specialize (h (Mk_Id GHC.Err.default
+                           w0
+                           GHC.Err.default
+                           GHC.Err.default
+                           GHC.Err.default
+                           GHC.Err.default)).
+      simpl in h.
+      replace (compare w0 w0) with Eq in h.
+      contradiction. cbn. symmetry. apply N.compare_refl.
+    - simpl. reflexivity.
+  Qed.
 
   Lemma is_empty_2 : forall s : t, is_empty s = true -> Empty s.
   Proof.
-    intros ????.
-    unfold In in *. destruct s. simpl in *.
-  Admitted.
+    move=>s. destruct s. simpl. destruct getUniqSet'. destruct i.
+    cbn. rewrite /Empty /In. cbn. rewrite /IntMap.null /IntMap.member.
+    simpl. move=>h a.
+    rewrite member_spec; [|eassumption]. intro.
+    rewrite -> null_spec in h; [|assumption]; subst.
+    simpl in H. destruct H. inversion H.
+  Qed.
 
   Lemma add_1 :
     forall (s : t) (x y : elt), E.eq x y -> In y (add x s).
   Proof.
     unfold E.eq, In.
     intros; subst.
-  Admitted.
+    destruct s, getUniqSet', i. cbn.
+    eapply insert_Desc; eauto.
+    intros. rewrite member_spec; [|eassumption].
+    specialize (H2 (realUnique y)).
+    exists x. rewrite H2.
+    assert (realUnique y == realUnique x).
+    { cbn. rewrite realUnique_eq. symmetry. assumption. }
+    rewrite H3. reflexivity.
+  Qed.
 
   Lemma add_2 : forall (s : t) (x y : elt), In y s -> In y (add x s).
   Proof.
-    intros.
-  Admitted.
+    move=>s. destruct s, getUniqSet', i. cbn.
+    rewrite /In. cbn. intros.
+    eapply insert_Desc; eauto.
+    intros. rewrite member_spec; [|eassumption].
+    rewrite -> member_spec in H; [|eassumption].
+    destruct H. specialize (H2 (realUnique y)).
+    destruct (realUnique y == realUnique x0).
+    - exists x0. rewrite H2. reflexivity.
+    - exists x1. rewrite H2 H. reflexivity.
+  Qed.
 
   Lemma add_3 :
     forall (s : t) (x y : elt), ~ E.eq x y -> In y (add x s) -> In y s.
   Proof.
-    intros.
-  Admitted.
+    move=>s. destruct s, getUniqSet', i. cbn.
+    rewrite /In. cbn. intros.
+    rewrite member_spec; [|eassumption].
+    rewrite -> member_spec in H0.
+    - destruct H0. exists x1. rewrite <- H0.
+      eapply insert_Desc; eauto.
+      intros. specialize (H3 (realUnique y)).
+      rewrite H3.
+      assert (realUnique y == realUnique x0 = false).
+      { cbn. rewrite realUnique_eq.
+        apply not_true_is_false. intro.
+        apply H. symmetry. apply H4. }
+      rewrite H4. reflexivity.
+    - apply insert_WF; assumption.
+  Qed.
 
   Lemma remove_1 :
     forall (s : t) (x y : elt), E.eq x y -> ~ In y (remove x s).
   Proof.
-  Admitted.
+    move=>s. destruct s, getUniqSet', i. cbn.
+    rewrite /In. cbn. intros.
+    eapply delete_Desc; eauto.
+    intros. rewrite member_spec; [|eassumption]. intro.
+    specialize (H2 (realUnique y)). destruct H3.
+    assert (realUnique y == realUnique x0).
+    { cbn. rewrite realUnique_eq. symmetry. assumption. }
+    rewrite H4 in H2. rewrite H2 in H3. inversion H3.
+  Qed.
 
   Lemma remove_2 :
     forall (s : t) (x y : elt), ~ E.eq x y -> In y s -> In y (remove x s).
   Proof.
-  Admitted.
+    move=>s. destruct s, getUniqSet', i. cbn.
+    rewrite /In. cbn. intros.
+    eapply delete_Desc; eauto.
+    intros. rewrite member_spec; [|eassumption].
+    rewrite -> member_spec in H0; [|eassumption]. destruct H0.
+    specialize (H3 (realUnique y)).
+    assert (realUnique y == realUnique x0 = false).
+    { cbn. rewrite realUnique_eq.
+      apply not_true_is_false. intro.
+      apply H. symmetry. assumption. }
+    rewrite H4 in H3. exists x1. rewrite H3 H0. reflexivity.
+  Qed.
 
   Lemma remove_3 :
     forall (s : t) (x y : elt), In y (remove x s) -> In y s.
   Proof.
-    intros.
-  Admitted.
+    move=>s. destruct s, getUniqSet', i. cbn.
+    rewrite /In. cbn. intros.
+    rewrite member_spec; [|eassumption].
+    rewrite -> member_spec in H.
+    - destruct H. exists x1. rewrite -H.
+      move: H. eapply delete_Desc; eauto.
+      intros. specialize (H1 (realUnique y)).
+      destruct (realUnique y == realUnique x0).
+      + rewrite H1 in H2. inversion H2.
+      + symmetry. assumption.
+    - eapply delete_Desc; eauto.
+  Qed.
 
   Lemma singleton_1 :
     forall x y : elt, In y (singleton x) -> E.eq x y.
   Proof.
-    intros.
-  Admitted.
+    rewrite /In. cbn. move=>x y.
+    destruct (realUnique y ?= realUnique x)%N eqn:Heq; intros.
+    - apply N.compare_eq in Heq.
+      rewrite -E.eqb_eq -realUnique_eq Heq.
+      apply N.eqb_refl.
+    - inversion H.
+    - inversion H.
+  Qed.
 
   Lemma singleton_2 :
     forall x y : elt, E.eq x y -> In y (singleton x).
   Proof.
-    intros.
-  Admitted.
+    rewrite /In. cbn. move=>x y. inversion 1.
+    rewrite -realUnique_eq in H1.
+    assert ((realUnique y ?= realUnique x)%N = Eq).
+    { apply N.compare_eq_iff. symmetry. apply N.eqb_eq; assumption. }
+    rewrite H0. reflexivity.
+  Qed.
 
   Lemma union_1 :
     forall (s s' : t) (x : elt), In x (union s s') -> In x s \/ In x s'.
   Proof.
-    intros.
-  Admitted.
+    rewrite /In. cbn. move=> s s'.
+    destruct s, getUniqSet', i, s', getUniqSet', i.
+    cbn. move=>e.
+    eapply union_Desc; eauto. move=>s HB _ Hsem.
+    rewrite !member_spec; try eassumption.
+    move=>[v Hs]. specialize (Hsem (realUnique e)).
+    destruct (sem x0 (realUnique e)) eqn:Hx0.
+    - simpl in Hsem. right. exists v0. assumption.
+    - simpl in Hsem. left. exists v. rewrite -Hsem; assumption.
+  Qed.
 
   Lemma union_2 :
     forall (s s' : t) (x : elt), In x s -> In x (union s s').
@@ -278,7 +410,7 @@ Module VarSetFSet <: WSfun(Var_as_DT) <: WS.
     intros.
     destruct s, s'.
   Admitted.
-  
+
   Lemma inter_3 :
     forall (s s' : t) (x : elt), In x s -> In x s' -> In x (inter s s').
   Proof.
@@ -386,14 +518,14 @@ Module VarSetFSet <: WSfun(Var_as_DT) <: WS.
   Definition lt (s s' : Var) := GHC.Base.compare s s' = Lt.
 
   (* Equality must be decidable, but doesn't necessarily need to be Coq
-     equality. For VarSets, in fact, that is not the case. 
-     
-     However, GHC does not include an equality instance for VarSets, 
-     so we don't actually need to define this. 
+     equality. For VarSets, in fact, that is not the case.
+
+     However, GHC does not include an equality instance for VarSets,
+     so we don't actually need to define this.
    *)
 
-  Definition equal  : t -> t -> bool := 
-    fun x y : t => 
+  Definition equal  : t -> t -> bool :=
+    fun x y : t =>
       match x with
       | UniqSet.Mk_UniqSet u =>
         match y with
@@ -407,7 +539,7 @@ Module VarSetFSet <: WSfun(Var_as_DT) <: WS.
         end
       end.
 
-  (* These operations are part of the FSet interface but are not 
+  (* These operations are part of the FSet interface but are not
      supported by GHC VarSets. *)
 
   Definition min_elt : t -> option elt := GHC.Err.default.
@@ -510,7 +642,7 @@ Module VarSetFSet <: WSfun(Var_as_DT) <: WS.
   Admitted.
 
   Lemma choose_3 (s1 s2 : t) (x1 x2 : elt) :
-    choose s1 = Some x1 -> 
+    choose s1 = Some x1 ->
     choose s2 = Some x2 ->
     Equal s1 s2         ->
     E.eq  x1 x2.
@@ -526,21 +658,21 @@ Export VarSetFSet.
 
 (* *********************************************************************** *)
 
-(** These two modules provide additional reasoning principles, proved in terms 
+(** These two modules provide additional reasoning principles, proved in terms
     of the basic signature. *)
 
 (** This functor derives additional facts from the interface. These facts are
     mainly the specifications of FSetInterface.S written using different styles:
-    equivalence and boolean equalities. 
+    equivalence and boolean equalities.
 
-    Notably, see tactic [set_iff]. 
+    Notably, see tactic [set_iff].
 *)
 
 Module VarSetFacts        := FSetFacts.WFacts_fun Var_as_DT VarSetFSet.
 Export VarSetFacts.
 
 (** This functor gives us properties about the boolean function specification
-    of sets. 
+    of sets.
 
     It adds some of these lemmas to a hint database called 'sets'.
 
@@ -549,27 +681,27 @@ Export VarSetFacts.
 Module VarSetEqProperties := FSetEqProperties.WEqProperties_fun Var_as_DT VarSetFSet.
 Export VarSetEqProperties.
 
-(** This functor gives us properties about the "prop" specification of 
+(** This functor gives us properties about the "prop" specification of
     sets. *)
 
 Module VarSetProperties   := FSetProperties.WProperties_fun Var_as_DT VarSetFSet.
-Export VarSetProperties. 
- 
+Export VarSetProperties.
+
 (** The [VarSetDecide] module provides the [fsetdec] tactic for
     solving facts about finite sets of vars. *)
 
 Module VarSetDecide      := FSets.FSetDecide.WDecide_fun Var_as_DT VarSetFSet.
-Export VarSetDecide. 
+Export VarSetDecide.
 
 
 (* *********************************************************************** *)
 (* The next part is taken from Metalib/FSetWeakNotin.v
 
    SCW note: I didn't want to draw in a dependency on Metalib before determining
-   whether the tactics are useful in this development. 
+   whether the tactics are useful in this development.
 
-   Furthermore, metalib assumes that Atom equality is =. But we need more 
-   flexibility for GHC. I had to edit some of the lemmas and proofs below 
+   Furthermore, metalib assumes that Atom equality is =. But we need more
+   flexibility for GHC. I had to edit some of the lemmas and proofs below
    because of that.
 *)
 (* *********************************************************************** *)
@@ -612,7 +744,7 @@ Lemma notin_add_3 :
   ~ E.eq x y ->
   ~ In y s ->
   ~ In y (add x s).
-Proof. 
+Proof.
   set_iff. tauto. Qed.
 
 Lemma notin_singleton_1 :
@@ -643,7 +775,7 @@ Proof. fsetdec. Qed.
 Lemma notin_remove_3 :
   E.eq x y ->
   ~ In y (remove x s).
-Proof. 
+Proof.
   set_iff. tauto.
  Qed.
 
@@ -786,7 +918,7 @@ End Notin.
 
 (* --------------------------------------------------------- *)
 
-(* Do we actually need this ??? It is never used in the code.  Why not the 
+(* Do we actually need this ??? It is never used in the code.  Why not the
    more extensional definition of equality between VarSets? *)
 
 (*
@@ -834,13 +966,13 @@ Proof. intros. unfold is_true. split.
 
 (* Note: ForallE and ExistsE require a condition on the predicate *)
 
-(* These lemmas relate the GHC VarSet operations to more general 
+(* These lemmas relate the GHC VarSet operations to more general
    operations on finite sets. *)
 
 Lemma emptyVarSet_empty : emptyVarSet = empty.
   reflexivity. Qed.
 
-(* need to swap argument order (should we use flip instead ??) *) 
+(* need to swap argument order (should we use flip instead ??) *)
 Lemma delVarSet_remove : forall x s, delVarSet s x = remove x s.
   reflexivity. Qed.
 
@@ -859,7 +991,7 @@ Lemma minusVarSet_diff : minusVarSet = diff.
 Lemma filterVarSet_filter : filterVarSet = filter.
   reflexivity. Qed.
 
-(* This tactic rewrites the boolean functions into the 
+(* This tactic rewrites the boolean functions into the
    set properties to make them suitable for fsetdec. *)
 
 Ltac set_b_iff :=
@@ -890,13 +1022,13 @@ Ltac set_b_iff :=
 
 Instance delVarSet_m :
   Proper (Equal ==> (fun x y => x == y) ==> Equal) delVarSet.
-Proof. 
+Proof.
   move => x1 y1 Eq1 x2 y2 Eq2.
   set_b_iff.
   eapply remove_m; eauto.
 Qed.
 
-Instance extendVarSet_m : 
+Instance extendVarSet_m :
   Proper (Equal ==> (fun x y => x == y) ==> Equal) extendVarSet.
 Proof.
   move => x1 y1 Eq1 x2 y2 Eq2.
@@ -904,7 +1036,7 @@ Proof.
   eapply add_m; eauto.
 Qed.
 
-Instance unionVarSet_m : 
+Instance unionVarSet_m :
   Proper (Equal ==> Equal ==> Equal) unionVarSet.
 Proof.
   move => x1 y1 Eq1 x2 y2 Eq2.
@@ -912,12 +1044,10 @@ Proof.
   eapply union_m; eauto.
 Qed.
 
-Instance minusVarSet_m : 
+Instance minusVarSet_m :
   Proper (Equal ==> Equal ==> Equal) minusVarSet.
 Proof.
   move => x1 y1 Eq1 x2 y2 Eq2.
   set_b_iff.
   eapply diff_m; eauto.
 Qed.
-
-
